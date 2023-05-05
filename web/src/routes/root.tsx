dd970c35d3978d213e59d3b2c1a214c00a6b1bae
David Cramer <dcramer@gmail.com>
2023-05-05 13:12:20 -0500

    Remove route error example

--- web/src/routes/root.tsx
+++ web/src/routes/root.tsx
@@ -1,35 +1,10 @@
-import {
-  Outlet,
-  isRouteErrorResponse,
-  useNavigate,
-  useRouteError,
-} from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import useAuth from "../hooks/useAuth";
 import Screen from "../components/screen";
 
 export default function Root() {
   const navigate = useNavigate();
-  const error = useRouteError();
-
-  if (isRouteErrorResponse(error)) {
-    if (error.status === 404) {
-      return <div>This page doesn't exist!</div>;
-    }
-
-    if (error.status === 401) {
-      return <div>You aren't authorized to see this</div>;
-    }
-
-    if (error.status === 503) {
-      return <div>Looks like our API is down</div>;
-    }
-
-    if (error.status === 418) {
-      return <div>🫖</div>;
-    }
-  }
-
   const { user, state } = useAuth();
 
   useEffect(() => {
