c44d63da869180f92a0666fd1d4a9385a47aaa6f
David Cramer <dcramer@gmail.com>
2023-05-06 11:58:06 -0500

    prod config fixes

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,31 +1,57 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      // Set it to `true` to export React component as default.
-      // Notice that it will overrides the default behavior of Vite.
-      exportAsDefault: false,
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
 
-      // svgr options: https://react-svgr.com/docs/options/
-      svgrOptions: {
-        // ...
-      },
+  return {
+    plugins: [
+      react(),
+      svgr({
+        // Set it to `true` to export React component as default.
+        // Notice that it will overrides the default behavior of Vite.
+        exportAsDefault: false,
 
-      // esbuild options, to transform jsx to js
-      esbuildOptions: {
-        // ...
-      },
+        // svgr options: https://react-svgr.com/docs/options/
+        svgrOptions: {
+          // ...
+        },
 
-      //  A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include. By default all svg files will be included.
-      include: "**/*.svg",
+        // esbuild options, to transform jsx to js
+        esbuildOptions: {
+          // ...
+        },
 
-      //  A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should ignore. By default no files are ignored.
-      exclude: "",
-    }),
-  ],
+        //  A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include. By default all svg files will be included.
+        include: "**/*.svg",
+
+        //  A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should ignore. By default no files are ignored.
+        exclude: "",
+      }),
+      sentryVitePlugin({
+        org: env.SENTRY_ORG,
+        project: env.SENTRY_PROJECT,
+
+        // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+        // and need `project:releases` and `org:read` scopes
+        authToken: env.SENTRY_AUTH_TOKEN,
+
+        sourcemaps: {
+          // Specify the directory containing build artifacts
+          assets: "./dist/**",
+        },
+
+        // Use the following option if you're on an SDK version lower than 7.47.0:
+        // include: "./dist",
+
+        // Optionally uncomment the line below to override automatic release name detection
+        // release: env.RELEASE,
+      }),
+    ],
+  };
 });
