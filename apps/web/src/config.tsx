c64045590b3d04d1b94cf487d102e2024a4a011d
David Cramer <dcramer@gmail.com>
2023-05-06 11:48:10 -0500

    Fix env name for api host

--- apps/web/src/config.tsx
+++ apps/web/src/config.tsx
@@ -3,10 +3,8 @@ const config = {
   DEBUG: import.meta.env.DEV,
   GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID || "",
   SENTRY_DSN: import.meta.env.VITE_SENTRY_DSN || "",
-
+  API_SERVER: import.meta.env.VITE_API_HOST || "http://localhost:4000",
   GITHUB_REPO: "https://github.com/dcramer/peated",
-
-  API_SERVER: import.meta.env.API_SERVER || "http://localhost:4000",
 };
 
 export default config;
