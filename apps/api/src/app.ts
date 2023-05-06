68075abb15ddd2dc9cf998e5d1c46ac34502d556
David Cramer <dcramer@gmail.com>
2023-05-06 12:02:09 -0500

    Add sentry to api

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -6,6 +6,17 @@ import FastifyMultipart from "@fastify/multipart";
 import { router } from "./routes";
 import config from "./config";
 
+import * as Sentry from "@sentry/node";
+
+Sentry.init({
+  dsn: config.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 const envToLogger: {
   [env: string]: any;
 } = {
@@ -38,6 +49,8 @@ export default async function buildFastify(options = {}) {
   app.register(router);
 
   app.setErrorHandler(function (error, request, reply) {
+    Sentry.captureException(error);
+
     if (error instanceof fastify.errorCodes.FST_ERR_BAD_STATUS_CODE) {
       // Log error
       this.log.error(error);
