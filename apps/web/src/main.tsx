ea4c569e9549878cbdce04f6f8017a7d562e1bec
David Cramer <dcramer@gmail.com>
2023-05-06 15:42:57 -0500

    Fix manifest

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -41,4 +41,4 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.unregister();
+ServiceWorkerRegistration.unregister();
