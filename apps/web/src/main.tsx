de244ae8c3ceee469bf6893f4ede489e00839ee6
David Cramer <dcramer@gmail.com>
2023-05-08 10:32:12 -0700

    Enable debug for sentry browser

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -14,6 +14,7 @@ import { register } from "./serviceWorkerRegistration";
 Sentry.init({
   dsn: config.SENTRY_DSN,
   release: config.VERSION,
+  debug: config.DEBUG,
 
   integrations: [
     new Sentry.BrowserTracing(),
