a36af634876153a892d4662530882e64a3439c96
David Cramer <dcramer@gmail.com>
2023-05-07 17:44:39 -0700

    We dont need no stinkin exporter

--- apps/api/src/instruments.ts
+++ apps/api/src/instruments.ts
@@ -5,17 +5,11 @@ import {
 } from "@sentry/opentelemetry-node";
 
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import otelApi from "@opentelemetry/api";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
 import { prisma } from "./lib/db";
 
 export const otelSdk = new NodeSDK({
-  // Existing config
-  traceExporter: new OTLPTraceExporter(),
   instrumentations: [getNodeAutoInstrumentations()],
-
-  // Sentry config
   spanProcessor: new SentrySpanProcessor(),
   textMapPropagator: new SentryPropagator(),
 });
