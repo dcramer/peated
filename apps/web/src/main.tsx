a50714efc9a1c629af406e18ff51460ce1c79719
David Cramer <dcramer@gmail.com>
2023-05-08 15:26:42 -0700

    Alright lets try it with javascript

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -51,6 +51,35 @@ const router = Sentry.wrapCreateBrowserRouter(createBrowserRouter)(
   createRoutes()
 );
 
+function initMobileControls() {
+  // https://stackoverflow.com/questions/37808180/disable-viewport-zooming-ios-10-safari
+  document.addEventListener(
+    "touchmove",
+    function (event) {
+      // @ts-ignore-next-line
+      if (event.scale !== 1) {
+        event.preventDefault();
+      }
+    },
+    false
+  );
+
+  var lastTouchEnd = 0;
+  document.addEventListener(
+    "touchend",
+    function (event) {
+      var now = new Date().getTime();
+      if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+      }
+      lastTouchEnd = now;
+    },
+    false
+  );
+}
+
+initMobileControls();
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
