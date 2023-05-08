343ce8867249de08c47d790ca80529687bf1ca03
David Cramer <dcramer@gmail.com>
2023-05-08 15:44:31 -0700

    Alright try suppressing zoom this way

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -52,30 +52,22 @@ const router = Sentry.wrapCreateBrowserRouter(createBrowserRouter)(
 );
 
 function initMobileControls() {
-  // https://stackoverflow.com/questions/37808180/disable-viewport-zooming-ios-10-safari
-  document.addEventListener(
-    "touchmove",
-    function (event) {
-      // @ts-ignore-next-line
-      if (event.scale !== 1) {
-        event.preventDefault();
-      }
-    },
-    false
-  );
+  document.addEventListener("gesturestart", function (e) {
+    e.preventDefault();
+    // @ts-ignore-next-line
+    document.body.style.zoom = 0.99;
+  });
 
-  var lastTouchEnd = 0;
-  document.addEventListener(
-    "touchend",
-    function (event) {
-      var now = new Date().getTime();
-      if (now - lastTouchEnd <= 300) {
-        event.preventDefault();
-      }
-      lastTouchEnd = now;
-    },
-    false
-  );
+  document.addEventListener("gesturechange", function (e) {
+    e.preventDefault();
+    // @ts-ignore-next-line
+    document.body.style.zoom = 0.99;
+  });
+  document.addEventListener("gestureend", function (e) {
+    e.preventDefault();
+    // @ts-ignore-next-line
+    document.body.style.zoom = 1;
+  });
 }
 
 initMobileControls();
