42487846a7555bde8aeee3bd07f178bb4491b4cc
David Cramer <dcramer@gmail.com>
2023-05-08 22:02:29 -0700

    Hide google auth if no creds

--- apps/web/src/routes/login.tsx
+++ apps/web/src/routes/login.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useState } from "react";
 import { LoaderFunction, useNavigate } from "react-router-dom";
-import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin } from "@react-oauth/google";
 import api, { ApiError } from "../lib/api";
 import useAuth from "../hooks/useAuth";
 import Layout from "../components/layout";
@@ -10,6 +10,7 @@ import TextField from "../components/textField";
 import classNames from "../lib/classNames";
 import Alert from "../components/alert";
 import Button from "../components/button";
+import config from "../config";
 
 type LoaderData = {};
 
@@ -56,14 +57,6 @@ const BasicLogin = () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <div className="relative mt-4 mb-2">
-        <div className="absolute inset-0 flex items-center" aria-hidden="true">
-          <div className="min-w-full border-t border-peated-light" />
-        </div>
-        <div className="relative flex justify-center">
-          <span className="bg-peated px-2 text-sm text-white">Or</span>
-        </div>
-      </div>
       <div className="mb-2">{error && <Alert>{error}</Alert>}</div>
       <TextField
         name="email"
@@ -99,9 +92,10 @@ const BasicLogin = () => {
   );
 };
 
-export default function Login() {
+const GoogleLogin = () => {
   const { login } = useAuth();
   const navigate = useNavigate();
+
   const [error, setError] = useState("");
 
   const googleLogin = useGoogleLogin({
@@ -125,31 +119,52 @@ export default function Login() {
   });
 
   return (
+    <div className="max-w-sm mx-auto">
+      {error && <Alert>{error}</Alert>}
+      <Button fullWidth onClick={() => googleLogin()}>
+        <svg
+          className="mr-2 -ml-1 w-4 h-4"
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fab"
+          data-icon="google"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 488 512"
+        >
+          <path
+            fill="currentColor"
+            d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+          ></path>
+        </svg>
+        Sign up with Google
+      </Button>
+    </div>
+  );
+};
+
+export default function Login() {
+  return (
     <Layout noHeader splash>
       <PeatedLogo color="white" />
 
       <div className="mt-8">
-        <div className="max-w-sm mx-auto">
-          {error && <Alert>{error}</Alert>}
-          <Button fullWidth onClick={() => googleLogin()}>
-            <svg
-              className="mr-2 -ml-1 w-4 h-4"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="google"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 488 512"
-            >
-              <path
-                fill="currentColor"
-                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-              ></path>
-            </svg>
-            Sign up with Google
-          </Button>
-        </div>
+        {config.GOOGLE_CLIENT_ID && (
+          <>
+            <GoogleLogin />{" "}
+            <div className="relative mt-4 mb-2">
+              <div
+                className="absolute inset-0 flex items-center"
+                aria-hidden="true"
+              >
+                <div className="min-w-full border-t border-peated-light" />
+              </div>
+              <div className="relative flex justify-center">
+                <span className="bg-peated px-2 text-sm text-white">Or</span>
+              </div>
+            </div>
+          </>
+        )}
         <BasicLogin />
       </div>
     </Layout>
