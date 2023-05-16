0dd6a1d315036c8f9ace5ec1fec827f3967f79a2
David Cramer <dcramer@gmail.com>
2023-05-16 10:49:01 -0700

    Fix position on floating button

--- apps/web/src/components/floatingButton.tsx
+++ apps/web/src/components/floatingButton.tsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 
 export default ({ to }: { to: string }) => {
   return (
-    <Link
-      type="button"
-      className="bg-highlight fixed bottom-8 right-8 rounded-full p-2 text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
-      to={to}
-    >
-      <PlusIcon className="h-8 w-8" aria-hidden="true" />
-    </Link>
+    <div className="absolute right-8">
+      <Link
+        type="button"
+        className="bg-highlight fixed bottom-8 z-10 rounded-full p-2 text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+        to={to}
+      >
+        <PlusIcon className="h-8 w-8" aria-hidden="true" />
+      </Link>
+    </div>
   );
 };
