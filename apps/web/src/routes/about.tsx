24b4827a1756f507588eb7bc154722f986505c25
David Cramer <dcramer@gmail.com>
2023-05-16 16:29:21 -0700

    Missed about page

---
+++ apps/web/src/routes/about.tsx
@@ -0,0 +1,30 @@
+import Layout from "../components/layout";
+import config from "../config";
+
+export default function About() {
+  return (
+    <Layout gutter>
+      <div className="prose">
+        <h1>About Peated</h1>
+        <p>
+          Peated, inspired by apps like Untapped and Vivino, aims to create a
+          rich social experience around tasting and collecting Whiskey. We
+          leverage modern and open source technology to go beyond the typical
+          experiences you see elsewhere.
+        </p>
+        <p>
+          Core to our goals is a spirit database heavily focused on reliability
+          and quality of data. This database has been bootstrapped by collating
+          various sources of data, and is curated by the community. Most
+          importantly, its available via a modern web API, the same API which
+          powers the application experiences.
+        </p>
+        <p>
+          Peated was started by{" "}
+          <a href="https://twitter.com/zeeg">David Cramer</a> and is{" "}
+          <a href={config.GITHUB_REPO}>Open Source on GitHub</a>.
+        </p>
+      </div>
+    </Layout>
+  );
+}
