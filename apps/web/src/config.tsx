137dc2d8016cac42b3eb947901794f26fae5e3ed
Luca Forstner <luca.forstner@sentry.io>
2023-05-22 10:29:55 +0200

    build(web): Add fixed version of Sentry vite plugin

--- apps/web/src/config.tsx
+++ apps/web/src/config.tsx
@@ -2,10 +2,10 @@ const config = {
   ENV: import.meta.env.PROD ? "production" : "development",
   DEBUG: import.meta.env.DEV,
   GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID || "",
-  SENTRY_DSN: import.meta.env.VITE_SENTRY_DSN || "",
+  SENTRY_DSN: import.meta.env.VITE_SENTRY_DSN,
   API_SERVER: import.meta.env.VITE_API_SERVER || "http://localhost:4000",
 
-  VERSION: import.meta.env.VITE_VERSION || "",
+  VERSION: import.meta.env.VITE_VERSION,
   GITHUB_REPO: "https://github.com/dcramer/peated",
   DISCORD_LINK: "https://discord.gg/d7GFPfy88Z",
 };
