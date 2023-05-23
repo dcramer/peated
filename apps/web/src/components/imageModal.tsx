855a9273693201d525628a988c9685f3a296b382
David Cramer <dcramer@gmail.com>
2023-05-23 10:45:52 -0700

    Move image handler to panel

--- apps/web/src/components/imageModal.tsx
+++ apps/web/src/components/imageModal.tsx
@@ -12,12 +12,13 @@ export function ImageModal({
   return (
     <Dialog open={open} as="div" className="dialog" onClose={setOpen}>
       <Dialog.Overlay className="fixed inset-0" />
-      <Dialog.Panel className="dialog-panel flex flex-col items-center justify-center px-4 pb-4 pt-5 sm:p-6">
-        <img
-          src={image}
-          className="max-h-full max-w-full"
-          onClick={() => setOpen(false)}
-        />
+      <Dialog.Panel
+        className="dialog-panel flex flex-col items-center justify-center px-4 pb-4 pt-5 sm:p-6"
+        onClick={() => {
+          setOpen(false);
+        }}
+      >
+        <img src={image} className="max-h-full max-w-full" />
       </Dialog.Panel>
     </Dialog>
   );
