115e2d10ac1f0c7add1800e94d6fb4868a305e77
David Cramer <dcramer@gmail.com>
2023-05-24 12:58:05 -0700

    Fix aggregate backfill

--- apps/api/migrations/0022_curved_flatman.sql
+++ apps/api/migrations/0022_curved_flatman.sql
@@ -15,7 +15,8 @@ END $$;
 ALTER TABLE "tasting" ALTER COLUMN "tags" SET DATA TYPE varchar(64)[];
 
 INSERT INTO "bottle_tag" ("bottle_id", "tag", "count")
-  SELECT "tasting"."bottle_id", unnest("tasting"."tags"), 1
+  SELECT "tasting"."bottle_id", unnest("tasting"."tags") as "tag", COUNT(*) as "count"
   FROM "tasting"
+  GROUP BY "bottle_id", "tag"
   ON CONFLICT ("bottle_id", "tag") DO UPDATE
-    SET "count" = "bottle_tag"."count" + 1;
+    SET "count" = excluded.count;
