84577c40152c9c463d5fdb82a13cd4f67d186220
David Cramer <dcramer@gmail.com>
2023-05-24 21:04:29 -0700

    Add invalidation to edit bottle

--- apps/web/src/routes/editBottle.tsx
+++ apps/web/src/routes/editBottle.tsx
@@ -1,28 +1,42 @@
 import { useNavigate, useParams } from "react-router-dom";
 
+import { BottleInputSchema } from "@peated/shared/schemas";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { z } from "zod";
 import BottleForm from "../components/bottleForm";
 import Spinner from "../components/spinner";
 import { useSuspenseQuery } from "../hooks/useSuspenseQuery";
 import api from "../lib/api";
 import { Bottle } from "../types";
 
+type FormSchemaType = z.infer<typeof BottleInputSchema>;
+
 export default function EditBottle() {
   const navigate = useNavigate();
   const { bottleId } = useParams();
+  const queryClient = useQueryClient();
   const { data: bottle } = useSuspenseQuery(
     ["bottles", bottleId],
     (): Promise<Bottle> => api.get(`/bottles/${bottleId}`),
     { cacheTime: 0 },
   );
 
+  const saveBottle = useMutation({
+    mutationFn: async (data: FormSchemaType) => {
+      await api.put(`/bottles/${bottleId}`, {
+        data,
+      });
+      await queryClient.invalidateQueries(["bottles", bottleId]);
+      navigate(`/bottles/${bottleId}`);
+    },
+  });
+
   if (!bottle) return <Spinner />;
+
   return (
     <BottleForm
-      onSubmit={async (data) => {
-        await api.put(`/bottles/${bottle.id}`, {
-          data,
-        });
-        navigate(`/bottles/${bottle.id}`);
+      onSubmit={(data) => {
+        saveBottle.mutate(data);
       }}
       initialData={bottle}
       title="Edit Bottle"
