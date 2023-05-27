2ad292c229db59e6042b5604f95f5450adcb70e5
David Cramer <dcramer@gmail.com>
2023-05-27 13:40:54 -0700

    Remove star rating field (unused)

--- apps/web/src/components/rating.tsx
+++ apps/web/src/components/rating.tsx
@@ -1,6 +1,4 @@
-import { RadioGroup } from "@headlessui/react";
 import { StarIcon } from "@heroicons/react/20/solid";
-import { useState } from "react";
 import classNames from "../lib/classNames";
 
 type RatingSize = "small" | "medium";
@@ -43,75 +41,5 @@ export const StaticRating = ({ value, size, className }: Props) => {
         ];
       })}
     </div>
-  );
-};
-
-export default ({
-  name,
-  id,
-  onChange,
-  required = false,
-  ...props
-}: {
-  id?: string;
-  name?: string;
-  value?: number | null | undefined;
-  onChange?: (value: number) => void;
-  required?: boolean;
-}) => {
-  const [value, setValue] = useState<number>(props.value || 0);
-
-  return (
-    <RadioGroup
-      id={id}
-      {...props}
-      onChange={(value) => {
-        setValue(value || 0);
-        onChange && onChange(value || 0);
-      }}
-    >
-      <div className="rating">
-        {[5, 4, 3, 2, 1].map((item) => {
-          return [
-            <RadioGroup.Option
-              key={item}
-              value={item}
-              className={({ active, checked }) =>
-                classNames(
-                  "cursor-pointer",
-                  "hover:text-slate-400",
-                  "rating-half-2",
-                  "peer",
-                  "peer-hover:text-highlight",
-                  active || checked || value >= item
-                    ? "text-highlight"
-                    : "text-slate-600",
-                )
-              }
-            >
-              <RadioGroup.Label as={StarIcon} className="h-full" />
-            </RadioGroup.Option>,
-            <RadioGroup.Option
-              key={item - 0.5}
-              value={item - 0.5}
-              className={({ active, checked }) =>
-                classNames(
-                  "cursor-pointer",
-                  "hover:text-slate-400",
-                  "rating-half-1",
-                  "peer",
-                  "peer-hover:text-highlight",
-                  active || checked || value >= item - 0.5
-                    ? "text-highlight"
-                    : "text-slate-600",
-                )
-              }
-            >
-              <RadioGroup.Label as={StarIcon} className="h-full" />
-            </RadioGroup.Option>,
-          ];
-        })}
-      </div>
-    </RadioGroup>
   );
 };
