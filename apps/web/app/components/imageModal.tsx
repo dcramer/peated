65e61e029567a9f8052d9ee64862d6ea587bfecb
David Cramer <dcramer@gmail.com>
2023-05-31 14:38:26 -0700

    Dont render imagemodal on server

--- apps/web/app/components/imageModal.tsx
+++ apps/web/app/components/imageModal.tsx
@@ -1,4 +1,5 @@
 import { Dialog } from "@headlessui/react";
+import { ClientOnly } from "remix-utils";
 
 export function ImageModal({
   image,
@@ -10,16 +11,20 @@ export function ImageModal({
   setOpen: (value: boolean) => void;
 }) {
   return (
-    <Dialog open={open} as="div" className="dialog" onClose={setOpen}>
-      <Dialog.Overlay className="fixed inset-0" />
-      <Dialog.Panel
-        className="dialog-panel flex flex-col items-center justify-center px-4 pb-4 pt-5 sm:p-6"
-        onClick={() => {
-          setOpen(false);
-        }}
-      >
-        <img src={image} className="max-h-full max-w-full" />
-      </Dialog.Panel>
-    </Dialog>
+    <ClientOnly>
+      {() => (
+        <Dialog open={open} as="div" className="dialog" onClose={setOpen}>
+          <Dialog.Overlay className="fixed inset-0" />
+          <Dialog.Panel
+            className="dialog-panel flex flex-col items-center justify-center px-4 pb-4 pt-5 sm:p-6"
+            onClick={() => {
+              setOpen(false);
+            }}
+          >
+            <img src={image} className="max-h-full max-w-full" />
+          </Dialog.Panel>
+        </Dialog>
+      )}
+    </ClientOnly>
   );
 }
