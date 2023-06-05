fdd4933c4b00b78fa4dc6250310bcf38514b9bde
David Cramer <dcramer@gmail.com>
2023-06-04 19:23:53 -0700

    Improve type hint for toBlob

--- apps/web/app/lib/blobs.ts
+++ apps/web/app/lib/blobs.ts
@@ -1,5 +1,5 @@
 export const toBlob = (canvas: HTMLCanvasElement) => {
-  return new Promise((resolve, reject) => {
+  return new Promise<Blob | null>((resolve, reject) => {
     canvas.toBlob(async (blob) => {
       resolve(blob);
     });
