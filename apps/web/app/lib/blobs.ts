6eb18e9572338fc885d58ced3c0a4eafed007613
David Cramer <dcramer@gmail.com>
2023-06-14 14:13:02 -0700

    Switch to pica to try and resolve filesize issues

--- apps/web/app/lib/blobs.ts
+++ apps/web/app/lib/blobs.ts
@@ -1,15 +1,16 @@
+import pica from "pica";
+
 export const toBlob = (
   canvas: HTMLCanvasElement,
   quality = 1,
   type = "image/webp",
 ) => {
+  const p = new pica();
   return new Promise<Blob | null>((resolve, reject) => {
-    canvas.toBlob(
-      async (blob) => {
-        resolve(blob);
-      },
-      type,
-      quality,
-    );
+    // const offScreenCanvas = document.createElement("canvas");
+    // offScreenCanvas.width = canvas.width;
+    // offScreenCanvas.height = canvas.height;
+
+    p.toBlob(canvas, type, quality).then((blob) => resolve(blob));
   });
 };
