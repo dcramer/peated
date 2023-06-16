680b8b81fc1abfc3448bf5f417152b2cad5d3abe
David Cramer <dcramer@gmail.com>
2023-06-16 15:03:41 -0700

    Add basics of Wooden Cork scraper

--- apps/scraper/src/price-scraper/woodencork.ts
+++ apps/scraper/src/price-scraper/woodencork.ts
@@ -18,10 +18,15 @@ function absoluteUrl(url: string, baseUrl: string) {
   return `${urlParts.origin}${url};`;
 }
 
+function removeBottleSize(name: string) {
+  return name.replace(/\([^)]+\)$/, "");
+}
+
 function parsePrice(value: string) {
   // $XX.YY
-  if (value.indexOf("$") !== 0)
-    throw new Error(`Invalid price value: ${value}`);
+  if (value.indexOf("$") !== 0) {
+    return;
+  }
 
   return parseInt(value.substring(1).split(".").join(""), 10);
 }
@@ -32,12 +37,31 @@ async function scrapeProducts(
 ) {
   const data = await getUrl(url);
   const $ = cheerio(data);
-  $("#main article").each(async (_, el) => {
-    const name = $("h2.title__2RoYeYuO > a", el).first().text();
-    if (!name) throw new Error("Unable to identify Product Name");
-    const productUrl = $("h2.title__2RoYeYuO > a", el).first().attr("href");
+  $("#CollectionAjaxContent div.grid-item").each(async (_, el) => {
+    const name = $("div.grid-product__title", el).first().text();
+    if (!name) {
+      console.warn("Unable to identify Product Name");
+      return;
+    }
+    const productUrl = $("a.grid-item__link", el).first().attr("href");
     if (!productUrl) throw new Error("Unable to identify Product URL");
-    const price = parsePrice($("span.price__1JvDDp_x", el).first().text());
+    const price = parsePrice(
+      $("span.grid-product__price--current > span.visually-hidden", el)
+        .first()
+        .text(),
+    );
+    if (!price) {
+      console.warn(
+        `Invalid price value: ${$(
+          "span.grid-product__price--current > span.visually-hidden",
+          el,
+        )
+          .first()
+          .text()}`,
+      );
+      return;
+    }
+    console.log(name);
     cb({
       name: normalizeBottleName(name),
       price,
@@ -48,23 +72,25 @@ async function scrapeProducts(
 }
 
 export async function main() {
+  // TODO: support pagination
   const products: Product[] = [];
-  await scrapeProducts(
-    "https://www.totalwine.com/spirits/scotch/c/000887?viewall=true&pageSize=120&aty=0,0,0,0",
-    async (product: Product) => {
-      products.push(product);
-    },
-  );
 
-  await scrapeProducts(
-    "https://www.totalwine.com/spirits/whiskey/c/9238919?viewall=true&pageSize=120&aty=0,0,0,0",
-    async (product: Product) => {
-      products.push(product);
-    },
-  );
+  let hasProducts = true;
+  let page = 1;
+  while (hasProducts) {
+    hasProducts = false;
+    await scrapeProducts(
+      `https://woodencork.com/collections/whiskey?page=${page}`,
+      async (product: Product) => {
+        products.push(product);
+        hasProducts = true;
+      },
+    );
+    page += 1;
+  }
 
   if (process.env.ACCESS_TOKEN) {
-    await submitStorePrices(1, products);
+    await submitStorePrices(2, products);
   } else {
     console.log("DRY RUN");
     console.log(`- ${products.length} products found`);
