4b21c912cc435a4f79b8422f86b2a02c556d74f0
David Cramer <dcramer@gmail.com>
2023-06-16 12:32:24 -0700

    Derp cant use ON CONFLICT in update

--- apps/api/migrations/0039_wild_invaders.sql
+++ apps/api/migrations/0039_wild_invaders.sql
@@ -2,11 +2,18 @@ ALTER TABLE "bottle" ADD COLUMN "full_name" varchar(255);
 
 CREATE UNIQUE INDEX IF NOT EXISTS "bottle_name_unq" ON "bottle" ("full_name");
 
-UPDATE "bottle" SET "full_name" = (
-    SELECT TRIM(TRAILING FROM CONCAT("entity"."name", ' ', "bottle"."name", ' ', "bottle"."series")) as "full_name"
-    FROM "entity"
-    WHERE "entity"."id" = "bottle"."brand_id" LIMIT 1
-) ON CONFLICT DO NOTHING;
+UPDATE "bottle"
+SET "full_name" = e."full_name"
+FROM (
+    SELECT TRIM(TRAILING FROM CONCAT("entity"."name", ' ', "bottle"."name", ' ', "bottle"."series")) as "full_name", "bottle"."id" as "bottle_id"
+    FROM "bottle"
+    JOIN "entity" ON "bottle"."brand_id" = "entity"."id"
+) AS e
+WHERE e."bottle_id" = "bottle"."id"
+  AND NOT EXISTS (
+    SELECT FROM "bottle" b2
+    WHERE b2."full_name" != e."full_name"
+  );
 
 DELETE FROM "change" WHERE "object_type" = "bottle" and "object_id" IN (
     SELECT "id" FROM "bottle" WHERE "full_name" IS NULL;
