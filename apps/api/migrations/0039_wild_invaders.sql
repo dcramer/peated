58324515ebb19d329308e8cbead1a78c8e9ad737
David Cramer <dcramer@gmail.com>
2023-06-16 13:00:47 -0700

    Ok this prob works

--- apps/api/migrations/0039_wild_invaders.sql
+++ apps/api/migrations/0039_wild_invaders.sql
@@ -5,17 +5,19 @@ CREATE UNIQUE INDEX IF NOT EXISTS "bottle_name_unq" ON "bottle" ("full_name");
 UPDATE "bottle"
 SET "full_name" = f."full_name"
 FROM (
-    SELECT TRIM(TRAILING FROM CONCAT(e."name", ' ', b."name", ' ', b."series")) as "full_name", b."id" as "bottle_id"
+    -- lol postgres
+    SELECT DISTINCT ON (TRIM(TRAILING FROM CONCAT(e."name", ' ', b."name", ' ', b."series"))) TRIM(TRAILING FROM CONCAT(e."name", ' ', b."name", ' ', b."series")) as "full_name", b."id" as "bottle_id"
     FROM "bottle" as b
-    JOIN "entity" as e ON b."brand_id" = e."id"
+    JOIN "entity" as e ON b."brand_id" = e."id";
 ) AS f
 WHERE f."bottle_id" = "bottle"."id"
   AND NOT EXISTS (
     SELECT FROM "bottle" b2
     WHERE b2."full_name" = f."full_name"
+      AND b2."id" != "bottle"."id"
   );
 
-DELETE FROM "change" WHERE "object_type" = 'bottle' and "object_id" IN (
+DELETE FROM "change" WHERE "objectq_type" = 'bottle' and "object_id" IN (
   SELECT "id" FROM "bottle" WHERE "full_name" IS NULL
 );
 
