f473c0275298ac93cde0bbc009f3933a3665e6e0
David Cramer <dcramer@gmail.com>
2023-06-16 12:06:37 -0700

    handle duplicated bottles

--- apps/api/migrations/0039_wild_invaders.sql
+++ apps/api/migrations/0039_wild_invaders.sql
@@ -1,11 +1,16 @@
 ALTER TABLE "bottle" ADD COLUMN "full_name" varchar(255);
 
+CREATE UNIQUE INDEX IF NOT EXISTS "bottle_name_unq" ON "bottle" ("full_name");
+
 UPDATE "bottle" SET "full_name" = (
     SELECT TRIM(TRAILING FROM CONCAT("entity"."name", ' ', "bottle"."name", ' ', "bottle"."series")) as "full_name"
     FROM "entity"
     WHERE "entity"."id" = "bottle"."brand_id" LIMIT 1
-);
+) ON CONFLICT ON CONSTRAINT "bottle_name_unq" DO NOTHING;
 
 ALTER TABLE "bottle" ALTER COLUMN "full_name" SET NOT NULL;
 
-CREATE UNIQUE INDEX IF NOT EXISTS "bottle_name_unq" ON "bottle" ("full_name");
+DELETE FROM "change" WHERE "object_type" = "bottle" and "object_id" IN (
+    SELECT "id" FROM "bottle" WHERE "full_name" IS NULL;
+);
+DELETE FROM "bottle" WHERE "full_name" IS NULL;
