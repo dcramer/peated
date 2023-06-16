d9b3f7b7280cadb6969096902304bf5ba91374cd
David Cramer <dcramer@gmail.com>
2023-06-16 12:36:36 -0700

    Bad syntax

--- apps/api/migrations/0039_wild_invaders.sql
+++ apps/api/migrations/0039_wild_invaders.sql
@@ -3,21 +3,22 @@ ALTER TABLE "bottle" ADD COLUMN "full_name" varchar(255);
 CREATE UNIQUE INDEX IF NOT EXISTS "bottle_name_unq" ON "bottle" ("full_name");
 
 UPDATE "bottle"
-SET "full_name" = e."full_name"
+SET "full_name" = f."full_name"
 FROM (
-    SELECT TRIM(TRAILING FROM CONCAT("entity"."name", ' ', "bottle"."name", ' ', "bottle"."series")) as "full_name", "bottle"."id" as "bottle_id"
-    FROM "bottle"
-    JOIN "entity" ON "bottle"."brand_id" = "entity"."id"
-) AS e
-WHERE e."bottle_id" = "bottle"."id"
+    SELECT TRIM(TRAILING FROM CONCAT(e."name", ' ', b."name", ' ', b."series")) as "full_name", b."id" as "bottle_id"
+    FROM "bottle" as b
+    JOIN "entity" as e ON b."brand_id" = e."id"
+) AS f
+WHERE f."bottle_id" = "bottle"."id"
   AND NOT EXISTS (
     SELECT FROM "bottle" b2
-    WHERE b2."full_name" != e."full_name"
+    WHERE b2."full_name" != f."full_name"
   );
 
-DELETE FROM "change" WHERE "object_type" = "bottle" and "object_id" IN (
-    SELECT "id" FROM "bottle" WHERE "full_name" IS NULL;
+DELETE FROM "change" WHERE "object_type" = 'bottle' and "object_id" IN (
+  SELECT "id" FROM "bottle" WHERE "full_name" IS NULL
 );
+
 DELETE FROM "bottle" WHERE "full_name" IS NULL;
 
 ALTER TABLE "bottle" ALTER COLUMN "full_name" SET NOT NULL;
