7f61ac438e1a868f5b814178214a8f234d7e5bcd
David Cramer <dcramer@gmail.com>
2023-06-29 19:14:11 -0700

    Move sidebar z-index to 10

--- apps/web/app/components/sidebar.tsx
+++ apps/web/app/components/sidebar.tsx
@@ -22,7 +22,7 @@ export default function Sidebar() {
 
   return (
     <>
-      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-64 lg:flex-col">
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-10 lg:flex lg:w-64 lg:flex-col">
         <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-slate-800 bg-slate-950 px-6 pb-4">
           <div className="text-highlight flex h-16 shrink-0 items-center hover:text-white">
             <HeaderLogo />
