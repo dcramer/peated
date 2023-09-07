43d1fcdc916624402ac8e902da86e2696928b053
David Cramer <dcramer@gmail.com>
2023-09-07 07:50:12 +0200

    Update point type to match new API

--- apps/api/src/db/columns.ts
+++ apps/api/src/db/columns.ts
@@ -1,10 +1,4 @@
-import type { ColumnBuilderConfig, ColumnConfig } from "drizzle-orm";
-import type {
-  AnyPgTable,
-  PgColumnBuilderHKT,
-  PgColumnHKT,
-} from "drizzle-orm/pg-core";
-import { PgColumn, PgColumnBuilder } from "drizzle-orm/pg-core";
+import { customType } from "drizzle-orm/pg-core";
 
 export class Point {
   lat: number;
@@ -22,50 +16,17 @@ export class Point {
 
 type GeographyType = Point | [number, number] | string;
 
-// SRID=4326;POINT(longitude latitude)
+export function geography<TData extends GeographyType = string>(name: string) {
+  return customType<{ data: TData; driverData: string }>({
+    dataType() {
+      return "geography";
+    },
 
-export class PgGeographyBuilder<
-  TData extends GeographyType = string,
-> extends PgColumnBuilder<
-  PgColumnBuilderHKT,
-  ColumnBuilderConfig<{ data: TData; driverParam: string }>
-> {
-  protected $pgColumnBuilderBrand = "PgGeographyBuilder";
-
-  build<TTableName extends string>(
-    table: AnyPgTable<{ name: TTableName }>,
-  ): PgGeography<TTableName, TData> {
-    return new PgGeography(table, this.config);
-  }
-}
-
-export class PgGeography<
-  TTableName extends string,
-  TData extends GeographyType,
-> extends PgColumn<
-  PgColumnHKT,
-  ColumnConfig<{ tableName: TTableName; data: TData; driverParam: string }>
-> {
-  constructor(
-    table: AnyPgTable<{ name: TTableName }>,
-    builder: PgGeographyBuilder<TData>["config"],
-  ) {
-    super(table, builder);
-  }
-
-  getSQLType(): string {
-    return "geography";
-  }
-
-  override mapToDriverValue(value: TData | GeographyType): string {
-    if (typeof value === "string") return value;
-    if (Array.isArray(value)) return `SRID=4326;POINT(${value[0]} ${value[1]})`;
-    return value.mapToDriverValue();
-  }
-}
-
-export function geography<T extends GeographyType = string>(
-  name: string,
-): PgGeographyBuilder<T> {
-  return new PgGeographyBuilder(name);
+    toDriver(value: TData): string {
+      if (typeof value === "string") return value;
+      if (Array.isArray(value))
+        return `SRID=4326;POINT(${value[0]} ${value[1]})`;
+      return value.mapToDriverValue();
+    },
+  })(name);
 }
