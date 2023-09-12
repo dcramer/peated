dc1e61cd4c7051ed244d2baaf05e03ba4c117678
David Cramer <dcramer@gmail.com>
2023-09-12 15:46:34 +0200

    Missing file from prior commit

---
+++ apps/web/app/routes/users.$username.favorites.tsx
@@ -0,0 +1,28 @@
+import type { Paginated } from "@peated/shared/types";
+import { useOutletContext } from "@remix-run/react";
+import { useQuery } from "@tanstack/react-query";
+import BottleTable from "~/components/bottleTable";
+import EmptyActivity from "~/components/emptyActivity";
+import useApi from "~/hooks/useApi";
+import type { Bottle, User } from "~/types";
+
+export default function ProfileCollections() {
+  const api = useApi();
+  const { user } = useOutletContext<{ user: User }>();
+
+  const { data } = useQuery({
+    queryKey: ["collections", "user", user.id],
+    queryFn: (): Promise<Paginated<Bottle>> =>
+      api.get(`/users/${user.id}/collections/default/bottles`),
+  });
+
+  return (
+    <>
+      {data && data.results.length ? (
+        <BottleTable bottleList={data.results} rel={data.rel} />
+      ) : (
+        <EmptyActivity>No favorites recorded yet.</EmptyActivity>
+      )}
+    </>
+  );
+}
