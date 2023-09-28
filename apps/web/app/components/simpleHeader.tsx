fda59d76b28ace0f8eb0d36db5c6a7d317ee48e1
David Cramer <dcramer@gmail.com>
2023-09-27 20:02:43 -0700

    Center children

--- apps/web/app/components/simpleHeader.tsx
+++ apps/web/app/components/simpleHeader.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 export default function SimpleHeader({ children }: { children?: ReactNode }) {
   return (
-    <h1 className="text-highlight border-highlight flex whitespace-nowrap border-b-4 px-1 py-4 text-sm font-medium">
+    <h1 className="text-highlight border-highlight flex justify-center whitespace-nowrap border-b-4 px-1 py-4 text-sm font-medium">
       {children}
     </h1>
   );
