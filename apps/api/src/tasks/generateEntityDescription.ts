8e2a989544abc25f4ee4ab259600b0fb4c7ccbf3
David Cramer <dcramer@gmail.com>
2023-10-03 22:58:28 -0700

    ok that error handler failed

--- apps/api/src/tasks/generateEntityDescription.ts
+++ apps/api/src/tasks/generateEntityDescription.ts
@@ -13,21 +13,21 @@ const MODEL = "gpt-3.5-turbo";
 // TODO: we can just pass in the information on if its a distillery or brand, and adjust the prompt accordingly?
 function generatePrompt(entityName: string) {
   return `
-We want to learn more about the subject "${entityName}", which may be whiskey distillery, a whiskey bottler, or brand of whiskey.
+We want to learn more about the entity "${entityName}", which may be whiskey distillery, a whiskey bottler, or brand of whiskey.
 
-If you cannot identify the subject, or are not certain it is real, respond with only the text "${UNKNOWN_ENTITY_MARKER}" and nothing else. Do not format the text in this case..
+If you cannot identify the entity, or are not certain it is real, respond with only the text "${UNKNOWN_ENTITY_MARKER}" and nothing else. Do not format the text in this case..
 
 Write a 200 word description. Focus on the history & origin, when it was founded, and what makes it different than its competitors.
 
 With all output, apply the following rules:
 
-- Describe the subject as a distiller, bottler, or brand, whichever one it primarily is. Do not describe it as a "subject".
+- Describe the entity as a distiller, bottler, or brand, whichever one it primarily is. Do not describe it as a "entity".
 - Be entirely truthful and use only facts.
-- Do not use the name of the subject in the description.
+- Do not use the name of the entity in the description.
 - Format all text with markdown, and not use any headings.
 - Keep the tone of the description somewhat formal, and without opinion.
-- Do not format the subjects name using quotation marks.
-- If the subject is from Scotland, spell Whiskey as "Whisky".
+- Do not format the entity name using quotation marks.
+- If the entity is from Scotland, spell Whiskey as "Whisky".
 `;
 }
 
