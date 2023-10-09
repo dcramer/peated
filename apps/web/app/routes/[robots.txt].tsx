386dff3ce8699b397902886c869cf1aa85a2a3be
David Cramer <dcramer@gmail.com>
2023-10-08 18:40:00 -0600

    Remove newlines

--- apps/web/app/routes/[robots.txt].tsx
+++ apps/web/app/routes/[robots.txt].tsx
@@ -1,10 +1,10 @@
+const robotText = `User-agent: *
+Allow: /
+`;
+
 export const loader = () => {
   // handle "GET" request
   // set up our text content that will be returned in the response
-  const robotText = `
-      User-agent: *
-      Allow: /
-      `;
   // return the text content, a status 200 success response, and set the content type to text/plain
   return new Response(robotText, {
     status: 200,
