696b0147bb9db73e7c4ba95434b733a85ac8ec19
David Cramer <dcramer@gmail.com>
2023-10-11 18:19:34 -0700

    Fix command on migrate

--- terraform/jobs.tf
+++ terraform/jobs.tf
@@ -15,7 +15,8 @@ resource "google_cloud_run_v2_job" "migrate_database" {
       }
 
       containers {
-        image = "us-central1-docker.pkg.dev/${data.google_project.project.project_id}/${google_artifact_registry_repository.peated.name}/api"
+        image   = "us-central1-docker.pkg.dev/${data.google_project.project.project_id}/${google_artifact_registry_repository.peated.name}/api"
+        command = "npm run db:migrate"
 
         resources {
           limits = {
