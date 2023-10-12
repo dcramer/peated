47fc3dad550a85ae4295588b121687d57d759874
David Cramer <dcramer@gmail.com>
2023-10-11 19:37:24 -0700

    Missing build args

--- terraform/scraper.tf
+++ terraform/scraper.tf
@@ -14,31 +14,6 @@ resource "google_cloud_run_v2_service" "scraper" {
         name  = "GOOGLE_CLIENT_ID"
         value = "721909483682-uk3befic1j1krv3drig2puu30v1i4v48.apps.googleusercontent.com"
       }
-
-      env {
-        name  = "CORS_HOST"
-        value = "https://peated.app"
-      }
-
-      env {
-        name  = "URL_PREFIX"
-        value = "https://api.peated.app"
-      }
-
-      env {
-        name  = "USE_GCS_STORAGE"
-        value = "1"
-      }
-
-      env {
-        name  = "GCS_BUCKET_NAME"
-        value = google_storage_bucket.peated.name
-      }
-
-      env {
-        name  = "GCS_BUCKET_PATH"
-        value = "uploads"
-      }
     }
   }
 
