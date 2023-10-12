cec3ec565199d7cd455ae624466f7e75724f491d
David Cramer <dcramer@gmail.com>
2023-10-12 09:57:13 -0700

    Disable scraper service

--- terraform/jobs.tf
+++ terraform/jobs.tf
@@ -62,3 +62,45 @@ resource "google_cloud_run_v2_job" "migrate_database" {
 
   depends_on = [google_sql_database_instance.main]
 }
+
+# TODO: this is really complicated on CloudRun so lets just move this to Faktory ASAP
+# resource "google_cloud_run_v2_job" "scraper" {
+#   name     = "scraper"
+#   location = var.region
+
+#   template {
+#     parallelism = 1
+
+#     template {
+#       # max_retries = 1
+#       timeout = 3600 # basically just reboot the process every hour
+
+#       containers {
+#         image = "us-central1-docker.pkg.dev/${data.google_project.project.project_id}/${google_artifact_registry_repository.peated.name}/scraper:latest"
+
+#         env {
+#           name  = "GOOGLE_CLIENT_ID"
+#           value = "721909483682-uk3befic1j1krv3drig2puu30v1i4v48.apps.googleusercontent.com"
+#         }
+
+#         resources {
+#           limits = {
+#             cpu    = "1000m"
+#             memory = "1Gi"
+#           }
+#         }
+#       }
+#     }
+#   }
+
+#   timeouts {}
+
+#   lifecycle {
+#     ignore_changes = [
+#       launch_stage,
+#     ]
+#   }
+
+#   depends_on = [google_cloud_run_v2_service.api]
+
+# }
