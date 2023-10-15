90f756a3d064b23206551043e360360ae6dea3d5
David Cramer <dcramer@gmail.com>
2023-10-14 17:55:38 -0700

    fix: Remove minimum constraint on year

--- packages/shared/schemas/entities.ts
+++ packages/shared/schemas/entities.ts
@@ -18,11 +18,7 @@ export const EntitySchema = z.object({
   id: z.number(),
   name: z.string().trim().min(1, "Required"),
   description: z.string().nullable().optional(),
-  yearEstablished: z
-    .number()
-    .gte(1500)
-    .lte(new Date().getFullYear())
-    .nullable(),
+  yearEstablished: z.number().lte(new Date().getFullYear()).nullable(),
   country: z.string().trim().nullable(),
   region: z.string().trim().nullable(),
   type: z.array(EntityTypeEnum),
