c20acfdcdd6d88d663308eb8139657fd1d2de0a3
David Cramer <dcramer@gmail.com>
2023-10-17 16:54:51 -0700

    feat: Add new service for new domain

--- terraform/web.tf
+++ terraform/web.tf
@@ -3,6 +3,31 @@ module "web-service" {
   name   = "web"
   image  = "us-central1-docker.pkg.dev/${data.google_project.project.project_id}/${google_artifact_registry_repository.peated.name}/web"
 
+  domains = ["peated.app", "staging.peated.app"]
+  port    = 3000
+
+  healthcheck = {
+    path = "/healthcheck"
+  }
+
+  k8s_service_account = module.gke_workload_identity.k8s_service_account_name
+
+  cloud_sql_instance = module.db-main.connection_name
+
+  env = {
+    GOOGLE_CLIENT_ID = var.google_client_id
+    SENTRY_DSN       = var.sentry_dsn
+    SESSION_SECRET   = data.google_secret_manager_secret_version.session_secret.secret_data
+  }
+
+  depends_on = [module.db-main]
+}
+
+module "peated-web-service" {
+  source = "./modules/service"
+  name   = "peated-web"
+  image  = "us-central1-docker.pkg.dev/${data.google_project.project.project_id}/${google_artifact_registry_repository.peated.name}/web"
+
   domains = ["peated.app", "staging.peated.app", "peated.com", "staging.peated.com", "www.peated.com"]
   port    = 3000
 
