709dd3c5532da9745243e0d5b8c3de9e2fca0c41
David Cramer <dcramer@gmail.com>
2023-10-17 15:26:49 -0700

    feat: Add peated.com

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -82,3 +82,56 @@ module "dns-private-zone" {
     },
   ]
 }
+
+
+module "peated-com-dns" {
+  source     = "terraform-google-modules/cloud-dns/google"
+  version    = "~> 5.1.0"
+  project_id = var.project_id
+  type       = "public"
+  name       = "peated-com"
+  domain     = "peated.com."
+
+  recordsets = [
+    {
+      name = "www"
+      type = "CNAME"
+      ttl  = 3600
+      records = [
+        "peated.com.",
+      ]
+    },
+    {
+      name = "api"
+      type = "A"
+      ttl  = 300
+      records = [
+        module.api-service.public_ip,
+      ]
+    },
+    {
+      name = ""
+      type = "A"
+      ttl  = 300
+      records = [
+        module.web-service.public_ip,
+      ]
+    },
+    {
+      name = "api.staging"
+      type = "A"
+      ttl  = 300
+      records = [
+        module.api-service.public_ip,
+      ]
+    },
+    {
+      name = "staging"
+      type = "A"
+      ttl  = 300
+      records = [
+        module.web-service.public_ip,
+      ]
+    },
+  ]
+}
