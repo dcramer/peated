ab7fd59c1c3fab4e246559447d807b0a87a2db99
David Cramer <dcramer@gmail.com>
2023-10-17 22:53:30 -0700

    ref: Remove unused helm provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -16,10 +16,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.11.0"
-    }
   }
 }
 
@@ -48,19 +44,3 @@ provider "kubernetes" {
   )
 }
 
-# XXX: do we really have to duplicate these
-provider "helm" {
-  kubernetes {
-    # config_path = "kubeconfig"
-    host  = "https://${module.gke.endpoint}"
-    token = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(
-      module.gke.ca_certificate,
-    )
-    # exec {
-    #   api_version = "client.authentication.k8s.io/v1beta1"
-    #   args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-    #   command     = "aws"
-    # }
-  }
-}
