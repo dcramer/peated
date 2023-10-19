116bbab629f06c69c03414020af8eb26cc015e5a
David Cramer <dcramer@gmail.com>
2023-10-19 15:03:05 -0700

    You know what this is fine

--- apps/web/app/routes/[_health].tsx
+++ apps/web/app/routes/[_health].tsx
@@ -1,7 +1,6 @@
 // learn more: https://fly.io/docs/reference/configuration/#services-http_checks
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import type { SitemapFunction } from "remix-sitemap";
-import { logError } from "~/lib/log";
 
 export const sitemap: SitemapFunction = () => ({
   exclude: true,
@@ -11,18 +10,5 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const host =
     request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
 
-  try {
-    const url = new URL("/", `http://${host}`);
-    // if we can connect to the database and make a simple query
-    // and make a HEAD request to ourselves, then we're good.
-    await Promise.all([
-      fetch(url.toString(), { method: "HEAD" }).then((r) => {
-        if (!r.ok) return Promise.reject(r);
-      }),
-    ]);
-    return new Response("OK");
-  } catch (error: unknown) {
-    logError(error);
-    return new Response("ERROR", { status: 500 });
-  }
+  return new Response("OK");
 };
