9dfe037bf95cef68abaf6ff8b038d098423c8fff
David Cramer <dcramer@gmail.com>
2023-10-22 18:26:45 -0700

    Correct truncation to resolve mobile issues

--- apps/web/app/components/bottleCard.tsx
+++ apps/web/app/components/bottleCard.tsx
@@ -22,7 +22,7 @@ export const PreviewBottleCard = ({
   return (
     <div className="bg-highlight flex items-center space-x-4 p-3 text-black sm:px-5 sm:py-4">
       <div className="flex-1 space-y-1">
-        <h4 className="block max-w-[260px] truncate font-semibold leading-6 sm:max-w-[480px]">
+        <h4 className="block max-w-[250px] truncate font-semibold leading-6 sm:max-w-[480px]">
           {data.name}
         </h4>
         <div className="text-sm">{brand ? brand.name : "Unknown Bottle"}</div>
