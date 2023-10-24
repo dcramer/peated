c989a36fe31a34a72a8a7c0e3a3f469afe8030fa
David Cramer <dcramer@gmail.com>
2023-10-24 10:44:56 -0700

    ref: Improve Sentry handling to include body, headers, and transaction names

--- apps/api/src/sentryPlugin.ts
+++ apps/api/src/sentryPlugin.ts
@@ -1,28 +1,27 @@
 import * as Sentry from "@sentry/node-experimental";
+import { isString, normalize } from "@sentry/utils";
 import fastifyPlugin from "fastify-plugin";
 
-export interface SentryContext {
-  transaction: any;
-}
-
-declare module "fastify" {
-  interface FastifyRequest {
-    _sentryContext: SentryContext;
-  }
-}
-
 export default fastifyPlugin(async (fastify, options) => {
-  fastify.decorateRequest("_sentryContext", null);
-
   fastify.addHook("preHandler", async (request) => {
     Sentry.configureScope((scope) =>
       scope.addEventProcessor((event) => {
         try {
+          event.transaction = `${request.method} ${request.routeOptions.url}`;
+          event.transaction_info = {
+            source: "url",
+          };
           event.request = {
             method: request.method,
-            url: request.url,
-            // headers: request.headers,
+            url: `${request.protocol}://${request.hostname}${request.url}`,
+            headers: request.headers as Record<string, string>, // idgaf
             query_string: request.query as Record<string, any>,
+            data:
+              request.body !== undefined
+                ? isString(request.body)
+                  ? request.body
+                  : JSON.stringify(normalize(request.body))
+                : undefined,
           };
         } catch (err) {
           console.error(err);
