fe9ce4d864b95002c35368d4e0a6695606e7a493
David Cramer <dcramer@gmail.com>
2023-10-25 16:58:29 -0700

    Missing peated.com in orogins

--- apps/web/app/entry.client.tsx
+++ apps/web/app/entry.client.tsx
@@ -14,6 +14,7 @@ Sentry.init({
         "localhost",
         /^\//,
         /^https:\/\/api\.peated\.app/,
+        /^https:\/\/api\.peated\.com/,
       ],
       routingInstrumentation: Sentry.remixRouterInstrumentation(
         useEffect,
