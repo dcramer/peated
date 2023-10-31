77fcc881aabfc2340e2b81cdf9a50a599cceccca
David Cramer <dcramer@gmail.com>
2023-10-31 16:01:20 -0700

    Add Sentry feedback widget

--- apps/web/app/entry.client.tsx
+++ apps/web/app/entry.client.tsx
@@ -1,15 +1,16 @@
 import { RemixBrowser, useLocation, useMatches } from "@remix-run/react";
+import { Feedback } from "@sentry-internal/feedback";
 import * as Sentry from "@sentry/remix";
 import { startTransition, useEffect } from "react";
 import { hydrateRoot } from "react-dom/client";
-import config from "./config";
-
 import packageData from "../package.json";
+import config from "./config";
 
 Sentry.init({
   dsn: config.SENTRY_DSN,
   release: config.VERSION,
 
+  debug: true,
   integrations: [
     new Sentry.BrowserTracing({
       tracePropagationTargets: [
@@ -29,6 +30,23 @@ Sentry.init({
     new Sentry.Replay({
       maskAllText: true,
       blockAllMedia: true,
+    }),
+    new Feedback({
+      // buttonLabel: "Feedback",
+      // submitButtonLabel: "Send Feedback",
+      // formTitle: "Feedback 🥔",
+      // messagePlaceholder: "What's not working? 😢",
+      // showEmail: false,
+      themeLight: {
+        foreground: "#94a3b8", // slate-400
+        background: "#0f172a", // slate-900
+        backgroundHover: "#020617", // slate-950
+      },
+      themeDark: {
+        foreground: "#94a3b8", // slate-400
+        background: "#0f172a", // slate-900
+        backgroundHover: "#020617", // slate-950
+      },
     }),
   ],
 
