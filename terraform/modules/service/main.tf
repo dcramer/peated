508798c8dcdc7ba0b32153ab4217646476ec08b4
David Cramer <dcramer@gmail.com>
2023-10-31 16:38:46 -0700

    Revert to default max surge

--- terraform/modules/service/main.tf
+++ terraform/modules/service/main.tf
@@ -112,7 +112,7 @@ resource "kubernetes_deployment_v1" "default" {
     strategy {
       type = "RollingUpdate"
       rolling_update {
-        max_surge       = "1"
+        // max_surge       = "1"
         max_unavailable = "1"
       }
     }
