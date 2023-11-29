6d17aaa48d5972dbda5c56c1fde25125074c9161
David Cramer <dcramer@gmail.com>
2023-11-29 13:34:46 -0800

    Changes for Vite (still broken)

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,19 +1,28 @@
-// module.exports = {
-//     ignoredRouteFiles: ["**/.*"],
-//     appDirectory: "app",
-//     assetsBuildDirectory: "public/build",
-//     serverBuildPath: "build/index.js",
-//     serverDependenciesToBundle: [/^@peated\/.*/],
-//     serverModuleFormat: "cjs",
-//     publicPath: "/build/",
-//     postcss: true,
-//     tailwind: true,
-//   };
-
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    remix({
+      // ignoredRouteFiles: ["**/.*"],
+      // appDirectory: "app",
+      // assetsBuildDirectory: "public/build",
+      // serverBuildPath: "build/index.js",
+      // serverModuleFormat: "cjs",
+      // publicPath: "/build/",
+    }),
+    tsconfigPaths(),
+  ],
+  server: {
+    fs: {
+      // Restrict files that could be served by Vite's dev server.  Accessing
+      // files outside this directory list that aren't imported from an allowed
+      // file will result in a 403.  Both directories and files can be provided.
+      // If you're comfortable with Vite's dev server making any file within the
+      // project root available, you can remove this option.  See more:
+      // https://vitejs.dev/config/server-options.html#server-fs-allow
+      allow: ["app"],
+    },
+  },
 });
