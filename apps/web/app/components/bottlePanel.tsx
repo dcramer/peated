6ff2f905951b315ea74efe1da32020fb4674c18b
David Cramer <dcramer@gmail.com>
2023-12-20 11:24:47 -0800

    Improve bottle panel to include various stats/actions

--- apps/web/app/components/bottlePanel.tsx
+++ apps/web/app/components/bottlePanel.tsx
@@ -2,11 +2,18 @@ import { type Bottle } from "@peated/server/types";
 import BottleIcon from "@peated/web/components/assets/Bottle";
 import { Link } from "@remix-run/react";
 import { type ComponentPropsWithoutRef } from "react";
+import useAuth from "../hooks/useAuth";
 import { formatCategoryName } from "../lib/strings";
 import BottleMetadata from "./bottleMetadata";
+import BottlePriceHistory from "./bottlePriceHistory";
 import Button from "./button";
+import { ClientOnly } from "./clientOnly";
+import CollectionAction from "./collectionAction";
 import PageHeader from "./pageHeader";
+import QueryBoundary from "./queryBoundary";
+import ShareButton from "./shareButton";
 import SidePanel, { SidePanelHeader } from "./sidePanel";
+import SkeletonButton from "./skeletonButton";
 
 export default function BottlePanel({
   bottle,
@@ -15,7 +22,20 @@ export default function BottlePanel({
 }: {
   bottle: Bottle;
   tastingPath?: string;
-} & ComponentPropsWithoutRef<typeof SidePanel>) {
+} & Omit<ComponentPropsWithoutRef<typeof SidePanel>, "children">) {
+  const { user } = useAuth();
+
+  const stats = [
+    {
+      name: "Avg Rating",
+      value:
+        bottle.avgRating !== null
+          ? Math.round(bottle.avgRating * 100) / 100
+          : "",
+    },
+    { name: "Tastings", value: bottle.totalTastings.toLocaleString() },
+  ];
+
   return (
     <SidePanel {...props}>
       <SidePanelHeader>
@@ -50,13 +70,45 @@ export default function BottlePanel({
           }
         />
       </SidePanelHeader>
-      <Button
-        color="highlight"
-        size="small"
-        to={tastingPath ?? `/bottles/${bottle.id}/addTasting`}
-      >
-        Record Tasting
-      </Button>
+
+      <div className="my-8 flex justify-center gap-4 lg:justify-start">
+        {user && (
+          <QueryBoundary
+            loading={<SkeletonButton className="w-10" />}
+            fallback={() => null}
+          >
+            <CollectionAction bottle={bottle} />
+          </QueryBoundary>
+        )}
+
+        <Button
+          to={tastingPath ?? `/bottles/${bottle.id}/addTasting`}
+          color="primary"
+        >
+          Record a Tasting
+        </Button>
+
+        <ShareButton title={bottle.fullName} url={`/bottles/${bottle.id}`} />
+      </div>
+
+      <div className="my-6 grid grid-cols-3 items-center gap-3 text-center lg:grid-cols-4 lg:text-left">
+        {stats.map((stat) => (
+          <div key={stat.name}>
+            <div className="text-light leading-7">{stat.name}</div>
+            <div className="order-first text-3xl font-semibold tracking-tight lg:text-5xl">
+              {stat.value || "-"}
+            </div>
+          </div>
+        ))}
+        <div className="hidden lg:block">
+          <div className="text-light leading-7">Price</div>
+          <div className="flex items-center">
+            <ClientOnly fallback={<div className="h-[45px] animate-pulse" />}>
+              {() => <BottlePriceHistory bottleId={bottle.id} />}
+            </ClientOnly>
+          </div>
+        </div>
+      </div>
     </SidePanel>
   );
 }
