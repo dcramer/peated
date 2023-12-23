3f080162891737726d31461ae30e5915c99d9a98
David Cramer <dcramer@gmail.com>
2023-12-22 21:21:59 -0800

    Remove ignore

--- terraform/modules/faktory/main.tf
+++ terraform/modules/faktory/main.tf
@@ -203,8 +203,6 @@ resource "kubernetes_stateful_set_v1" "stateful_set" {
 
   lifecycle {
     ignore_changes = [
-      spec[0].template[0].spec[0].container[0].image,
-      spec[0].template[0].spec[0].security_context,
       spec[0].template[0].spec[0].toleration,
       metadata[0].annotations["autopilot.gke.io/resource-adjustment"],
       metadata[0].annotations["autopilot.gke.io/warden-version"],
