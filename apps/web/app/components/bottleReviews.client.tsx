cb2b78146d21fb4db6b3b298e607785784754b2b
David Cramer <dcramer@gmail.com>
2023-12-30 21:13:45 -0800

    Full width on mobile

--- apps/web/app/components/bottleReviews.client.tsx
+++ apps/web/app/components/bottleReviews.client.tsx
@@ -22,7 +22,7 @@ export default function BottleReviews({ bottleId }: { bottleId: number }) {
   return (
     <>
       <h3 className="text-highlight text-lg font-bold">The Critics</h3>
-      <ul className="-mx-2 grid w-2/3 grid-cols-2 md:w-1/2">
+      <ul className="-mx-2 grid grid-cols-2 sm:w-2/3 md:w-1/2">
         {results.map((r) => {
           return (
             <li
