21874c996433bc4937181d203420e0513438fc3c
David Cramer <dcramer@gmail.com>
2023-12-30 19:47:46 -0800

    For real fix the migration

--- apps/server/migrations/0074_amused_purple_man.sql
+++ apps/server/migrations/0074_amused_purple_man.sql
@@ -1,5 +1,5 @@
-INSERT INTO bottle_alias
-SELECT DISTINCT bottle_id, name
+INSERT INTO bottle_alias (name, bottle_id)
+SELECT DISTINCT ON (name) name, bottle_id
 FROM store_price
 ON CONFLICT (name) DO UPDATE
 SET bottle_id = bottle_alias.bottle_id
@@ -7,11 +7,18 @@ WHERE bottle_alias.bottle_id IS NULL;
 
 UPDATE store_price SET bottle_id = (SELECT bottle_id FROM bottle_alias WHERE name = store_price.name) WHERE bottle_id IS NULL;
 
-INSERT INTO bottle_alias
-SELECT DISTINCT bottle_id, name
+INSERT INTO bottle_alias (name, bottle_id)
+SELECT DISTINCT ON (name) name, bottle_id
 FROM review
 ON CONFLICT (name) DO UPDATE
 SET bottle_id = bottle_alias.bottle_id
 WHERE bottle_alias.bottle_id IS NULL;
 
 UPDATE review SET bottle_id = (SELECT bottle_id FROM bottle_alias WHERE name = review.name) WHERE bottle_id IS NULL;
+
+
+UPDATE bottle_alias
+SET bottle_id = (
+    SELECT id FROM bottles WHERE full_name = bottle_alias.name
+)
+WHERE bottle_id IS NULL;
