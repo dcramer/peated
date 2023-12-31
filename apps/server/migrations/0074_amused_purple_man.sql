4b7a95184ce60dfa6502fbe2ac706fdab8988ac7
David Cramer <dcramer@gmail.com>
2023-12-30 18:45:09 -0800

    Backfill bottle alias for store/review entries

---
+++ apps/server/migrations/0074_amused_purple_man.sql
@@ -0,0 +1,17 @@
+INSERT INTO bottle_alias
+SELECT bottle_id, name
+FROM store_price
+ON CONFLICT (name) DO UPDATE
+SET bottle_id = bottle_alias.bottle_id
+WHERE bottle_alias.bottle_id IS NULL;
+
+UPDATE store_price SET bottle_id = (SELECT bottle_id FROM bottle_alias WHERE name = store_price.name) WHERE bottle_id IS NULL;
+
+INSERT INTO bottle_alias
+SELECT bottle_id, name
+FROM review
+ON CONFLICT (name) DO UPDATE
+SET bottle_id = bottle_alias.bottle_id
+WHERE bottle_alias.bottle_id IS NULL;
+
+UPDATE review SET bottle_id = (SELECT bottle_id FROM bottle_alias WHERE name = review.name) WHERE bottle_id IS NULL;
