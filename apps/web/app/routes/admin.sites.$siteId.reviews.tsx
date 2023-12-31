b01588442f1d9375147a9e6defd94e80af2b7204
David Cramer <dcramer@gmail.com>
2023-12-30 18:41:20 -0800

    Pass cursor/limit as numbers

--- apps/web/app/routes/admin.sites.$siteId.reviews.tsx
+++ apps/web/app/routes/admin.sites.$siteId.reviews.tsx
@@ -14,9 +14,14 @@ export const loader: LoaderFunction = async ({ request, context, params }) => {
   invariant(params.siteId);
 
   const { searchParams } = new URL(request.url);
+  const numericFields = new Set(["cursor", "limit"]);
   const reviewList = await context.trpc.reviewList.query({
     site: params.siteId as ExternalSiteType,
-    ...Object.fromEntries(searchParams.entries()),
+    ...Object.fromEntries(
+      [...searchParams.entries()].map(([k, v]) =>
+        numericFields.has(k) ? [k, Number(v)] : [k, v === "" ? null : v],
+      ),
+    ),
   });
 
   return json({ reviewList });
