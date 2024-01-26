5d57fbafa2b2c7f3e77a2a0e2edd104ea26469cc
David Cramer <dcramer@gmail.com>
2024-01-26 10:49:46 -0800

    Periodically update TimeSince (fixes GH-134)

--- apps/web/app/components/timeSince.tsx
+++ apps/web/app/components/timeSince.tsx
@@ -1,5 +1,7 @@
 import dayjs from "dayjs";
 import DayJsRelativeTime from "dayjs/plugin/relativeTime";
+import { useState } from "react";
+import { useTimeout } from "usehooks-ts";
 
 dayjs.extend(DayJsRelativeTime);
 
@@ -7,13 +9,19 @@ export default function TimeSince({
   date,
   ...props
 }: { date: string | Date } & React.ComponentProps<"time">) {
+  const [value, setValue] = useState(dayjs(date).fromNow());
+
+  useTimeout(() => {
+    setValue(dayjs(date).fromNow());
+  }, 60000);
+
   if (!date) return null;
   return (
     <time
       dateTime={date instanceof Date ? date.toISOString() : date}
       {...props}
     >
-      {dayjs(date).fromNow()}
+      {value}
     </time>
   );
 }
