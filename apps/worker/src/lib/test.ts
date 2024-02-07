a156cf674b51a99e58d8f1ae4f3cf583ef9f275a
David Cramer <dcramer@gmail.com>
2024-02-07 14:33:38 -0800

    Convert buffer to string for tests

--- apps/worker/src/lib/test.ts
+++ apps/worker/src/lib/test.ts
@@ -2,7 +2,8 @@ import { readFile } from "fs/promises";
 import path from "path";
 
 export async function loadFixture(...paths: string[]) {
-  return await readFile(
+  const data = await readFile(
     path.join(__dirname, "..", "..", "__fixtures__", ...paths),
   );
+  return data.toString();
 }
