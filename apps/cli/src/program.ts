f8511e808f61a79c96989042a3ac010bbd0e415b
David Cramer <dcramer@gmail.com>
2024-02-25 09:58:39 -0800

    Draft (broken) tracing for commander

--- apps/cli/src/program.ts
+++ apps/cli/src/program.ts
@@ -1,13 +1,29 @@
+import * as Sentry from "@sentry/node-experimental";
 import { Command } from "commander";
 import { shutdownClient } from "../../server/src/jobs";
 
 const program = new Command();
 
-program
-  .name("bottles")
-  .description("CLI for assisting with Peated")
-  .hook("postAction", async () => {
-    await shutdownClient();
-  });
+(() => {
+  let span: any;
+
+  program
+    .name("peated")
+    .description("CLI for assisting with Peated")
+    .hook("preSubcommand", async (thisCommand, subcommand) => {
+      // const parent = Sentry.getActiveSpan();
+      span = Sentry.startInactiveSpan({
+        op: "command",
+        name: `commander.${subcommand.name()}`,
+      });
+    })
+    .hook("postAction", async (thisCommand) => {
+      await shutdownClient();
+      if (span) {
+        span.end();
+        span = null;
+      }
+    });
+})();
 
 export default program;
