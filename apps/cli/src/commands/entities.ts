051e42c8f8785e7f9eac4f1fdde40d61d3806de0
David Cramer <dcramer@gmail.com>
2024-03-02 16:13:34 -0800

    Add helper to fix entity stats

--- apps/cli/src/commands/entities.ts
+++ apps/cli/src/commands/entities.ts
@@ -1,6 +1,13 @@
 import program from "@peated/cli/program";
 import { db } from "@peated/server/db";
+import {
+  bottles,
+  bottlesToDistillers,
+  entities,
+  tastings,
+} from "@peated/server/db/schema";
 import { pushJob } from "@peated/server/jobs";
+import { sql } from "drizzle-orm";
 
 const subcommand = program.command("entities");
 
@@ -24,3 +31,38 @@ subcommand
       await pushJob("GenerateEntityDetails", { entityId: entity.id });
     }
   });
+
+subcommand.command("fix-stats").action(async () => {
+  await db.update(entities).set({
+    totalBottles: sql<number>`(
+      SELECT COUNT(*)
+      FROM ${bottles}
+      WHERE (
+        ${bottles.brandId} = ${entities.id}
+        OR ${bottles.bottlerId} = ${entities.id}
+        OR EXISTS(
+          SELECT FROM ${bottlesToDistillers}
+          WHERE ${bottlesToDistillers.bottleId} = ${bottles.id}
+          AND ${bottlesToDistillers.distillerId} = ${entities.id}
+        )
+      )
+    )`,
+    totalTastings: sql<number>`(
+      SELECT COUNT(*)
+      FROM ${tastings}
+      WHERE ${tastings.bottleId} IN (
+        SELECT ${bottles.id}
+        FROM ${bottles}
+        WHERE (
+          ${bottles.brandId} = ${entities.id}
+          OR ${bottles.bottlerId} = ${entities.id}
+          OR EXISTS(
+            SELECT FROM ${bottlesToDistillers}
+            WHERE ${bottlesToDistillers.bottleId} = ${bottles.id}
+            AND ${bottlesToDistillers.distillerId} = ${entities.id}
+          )
+        )
+        )
+    )`,
+  });
+});
