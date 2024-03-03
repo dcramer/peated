a1eb4b87cf71bc155e65f1c08c1baa868d8351af
David Cramer <dcramer@gmail.com>
2024-03-03 14:51:49 -0800

    Bad global update query

--- apps/worker/src/jobs/scheduleScrapers.ts
+++ apps/worker/src/jobs/scheduleScrapers.ts
@@ -1,7 +1,7 @@
 import { db } from "@peated/server/db";
 import { externalSites } from "@peated/server/db/schema";
 import { getJobForSite, pushJob } from "@peated/server/jobs";
-import { and, isNotNull, isNull, lte, or, sql } from "drizzle-orm";
+import { and, eq, isNotNull, isNull, lte, or, sql } from "drizzle-orm";
 
 export default async function scheduleScrapers() {
   const pending = await db
@@ -21,12 +21,15 @@ export default async function scheduleScrapers() {
     for (const site of pending) {
       await pushJob(...getJobForSite(site.type));
       // TODO: have the job update nextRunAt when it finishes
-      await tx.update(externalSites).set({
-        lastRunAt: sql`NOW()`,
-        nextRunAt: sql`NOW() + INTERVAL '${sql.raw(
-          `${site.runEvery} minutes`,
-        )}'`,
-      });
+      await tx
+        .update(externalSites)
+        .set({
+          lastRunAt: sql`NOW()`,
+          nextRunAt: sql`NOW() + INTERVAL '${sql.raw(
+            `${site.runEvery} minutes`,
+          )}'`,
+        })
+        .where(eq(externalSites.id, site.id));
     }
   });
 }
