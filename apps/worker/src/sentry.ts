c4395acec393d06d6746087e4013a71c18f72f9c
David Cramer <dcramer@gmail.com>
2024-03-10 19:24:39 -0700

    Kill profiling as its yelling at me about types

--- apps/worker/src/sentry.ts
+++ apps/worker/src/sentry.ts
@@ -1,5 +1,4 @@
 import * as Sentry from "@sentry/node-experimental";
-import { ProfilingIntegration } from "@sentry/profiling-node";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
@@ -8,7 +7,6 @@ Sentry.init({
     process.env.NODE_ENV === "production" ? "production" : "development",
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0,
-  integrations: [new ProfilingIntegration()],
   spotlight: process.env.NODE_ENV !== "production",
 });
 
