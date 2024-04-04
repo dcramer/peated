e839fa88e9c58642506566f0c1060ab758ee49f2
David Cramer <dcramer@gmail.com>
2024-04-04 11:49:17 -0700

    fix yrs old and optional age

--- src/server/api/resource/bottle/normalize.go
+++ src/server/api/resource/bottle/normalize.go
@@ -15,15 +15,6 @@ func NormalizeBottleName(name string, statedAge *uint) string {
 	// this is primarily targeting Scotch Malt Whiskey Society bottles
 	name = strings.TrimPrefix(name, "Cask No. ")
 
-	if statedAge == nil {
-		return name
-	}
-	ageAsStr := strconv.FormatUint(uint64(*statedAge), 10)
-
-	if statedAge != nil && name == ageAsStr {
-		return fmt.Sprintf("%s%s", ageAsStr, ageSuffix)
-	}
-
 	// remove newlines
 	name = strings.ReplaceAll(name, "\n", " ")
 
@@ -35,10 +26,18 @@ func NormalizeBottleName(name string, statedAge *uint) string {
 	yearsOldRe := regexp.MustCompile(`(?i)(\d+)[\s-]?years?(?:[\s-]old)?($|\s)`)
 	name = yearsOldRe.ReplaceAllString(name, "$1"+ageSuffix+"$2")
 
-	// // abberviated yr
-	// name = name
-	//   .replace(/(\d+)\s?yrs?\.?[\s-]old($|\s)/i, `$1${ageSuffix}$2`)
-	//   .replace(/(\d+)\s?yrs?\.?($|\s)/i, `$1${ageSuffix}$2`);
+	// "yrs old"
+	yrsOldRe := regexp.MustCompile(`(?i)(\d+)[\s-]?yrs?\.?(?:[\s-]old)?($|\s)`)
+	name = yrsOldRe.ReplaceAllString(name, "$1"+ageSuffix+"$2")
+
+	if statedAge == nil {
+		return name
+	}
+	ageAsStr := strconv.FormatUint(uint64(*statedAge), 10)
+
+	if statedAge != nil && name == ageAsStr {
+		return fmt.Sprintf("%s%s", ageAsStr, ageSuffix)
+	}
 
 	if strings.HasPrefix(name, fmt.Sprintf("%s ", ageAsStr)) {
 		prefixRe := regexp.MustCompile(`^` + regexp.QuoteMeta(ageAsStr))
