db711f18541bd92c489befee5a549673a8443fec
David Cramer <dcramer@gmail.com>
2024-04-07 09:42:37 -0700

    Correct error handling for top level commander

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -2,9 +2,13 @@
 
 import "./sentry";
 
+import { logError } from "@peated/server/lib/log";
 import "./commands";
 import program from "./program";
 
 export { program };
 
-program.parseAsync();
+program.parseAsync().catch((err) => {
+  logError(err);
+  process.exit(1);
+});
