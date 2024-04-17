c5a88dd0909224a7853ff4011be7a9e6ec3bfd98
David Cramer <dcramer@gmail.com>
2024-04-16 20:46:35 -0700

    Show tags on mobile

--- apps/web/app/components/tags.tsx
+++ apps/web/app/components/tags.tsx
@@ -6,36 +6,27 @@ export default function Tags({ tags }: { tags: string[] }) {
   if (!tags || !tags.length) return null;
   return (
     <div className="text-sm">
-      <div className="hidden lg:block">
-        <Separated separator=", ">
-          {tags.slice(0, 3).map((item) => (
-            <Link
-              key={item}
-              className="hover:underline"
-              to={`/bottles?tag=${encodeURIComponent(item)}`}
-            >
-              {item}
-            </Link>
-          ))}
-        </Separated>
-        {tags.length > 3 && (
-          <span>
-            , and{" "}
-            <Tooltip title={tags.join(", ")}>
-              <span className="underline decoration-dotted">
-                {tags.length - 3} more
-              </span>
-            </Tooltip>
-          </span>
-        )}
-      </div>
-      <div className="lg:hidden">
-        <Tooltip title={tags.join(", ")}>
-          <span className="underline decoration-dotted">
-            {tags.length} flavor note{tags.length !== 1 ? "s" : ""}
-          </span>
-        </Tooltip>
-      </div>
+      <Separated separator=", ">
+        {tags.slice(0, 3).map((item) => (
+          <Link
+            key={item}
+            className="hover:underline"
+            to={`/bottles?tag=${encodeURIComponent(item)}`}
+          >
+            {item}
+          </Link>
+        ))}
+      </Separated>
+      {tags.length > 3 && (
+        <span>
+          , and{" "}
+          <Tooltip title={tags.join(", ")}>
+            <span className="underline decoration-dotted">
+              {tags.length - 3} more
+            </span>
+          </Tooltip>
+        </span>
+      )}
     </div>
   );
 }
