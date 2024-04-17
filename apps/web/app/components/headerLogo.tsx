b36dc317a8895e59805d35241e63098c6dc90178
David Cramer <dcramer@gmail.com>
2024-04-16 20:58:39 -0700

    Add beta tag

--- apps/web/app/components/headerLogo.tsx
+++ apps/web/app/components/headerLogo.tsx
@@ -6,9 +6,12 @@ import PeatedLogo from "./assets/Logo";
 export default function HeaderLogo() {
   return (
     <>
-      <div className="logo hidden items-center sm:flex">
-        <Link to="/">
+      <div className="logo relative hidden sm:block">
+        <Link to="/" className="items-center sm:flex">
           <PeatedLogo className="h-8 w-auto" />
+          <div className="ml-2 mt-2 inline-block w-auto rounded bg-slate-700 px-2 py-1 text-xs font-medium lowercase text-white opacity-90">
+            Beta
+          </div>
         </Link>
       </div>
       <div className="logo flex sm:hidden">
