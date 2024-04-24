c6dc5b5cabed86aebb77c8a5815ae11645a09646
David Cramer <dcramer@gmail.com>
2024-04-24 09:18:24 -0700

    Draft code to fix trace propagation

--- apps/server/src/jobs/client.ts
+++ apps/server/src/jobs/client.ts
@@ -60,7 +60,7 @@ export async function pushJob(jobName: JobName, args?: any) {
       // pull out traceparent to forward to faktory job
       const activeContext = {};
       propagation.inject(context.active(), activeContext);
-
+      console.log(activeContext);
       try {
         await client.job(jobName, args, { traceContext: activeContext }).push();
       } catch (e) {
@@ -73,6 +73,11 @@ export async function pushJob(jobName: JobName, args?: any) {
   );
 }
 
+type TraceContext = {
+  "sentry-trace"?: string;
+  baggage?: any;
+};
+
 export async function registerJob(jobName: JobName, jobFn: JobFunction) {
   faktory.register(jobName, instrumentedJob(jobName, jobFn));
 }
@@ -82,64 +87,60 @@ function instrumentedJob<T>(jobName: string, jobFn: JobFunction) {
   return async (...args: unknown[]) => {
     const jobId = cuid2.createId();
 
-    const activeContext = propagation.extract(
-      context.active(),
-      args.length > 1
-        ? (
-            args[1] as {
-              traceContext: {
-                traceId: string;
-                baggage: any;
-              };
-            }
-          ).traceContext
-        : {},
-    );
+    const activeContext: TraceContext =
+      args.length > 1 ? (args[1] as TraceContext) : {};
 
-    return context.with(activeContext, async () => {
-      return Sentry.withScope(async function (scope) {
-        scope.setContext("job", {
-          name: jobName,
-          id: jobId,
+    return Sentry.continueTrace(
+      {
+        sentryTrace: activeContext["sentry-trace"],
+        baggage: activeContext.baggage,
+      },
+      async () => {
+        return Sentry.withScope(async function (scope) {
+          scope.setContext("job", {
+            name: jobName,
+            id: jobId,
+          });
+          scope.setTransactionName(jobName);
+
+          // this is sentry's wrapper
+          return await Sentry.startSpan(
+            {
+              op: "process",
+              name: `faktory.${jobName.toLowerCase()}`,
+            },
+            async (span) => {
+              span.setAttribute("messaging.operation", "process");
+              span.setAttribute("messaging.system", "faktory");
+
+              console.log(`Running job [${jobName} - ${jobId}]`);
+              const start = new Date().getTime();
+              let success = false;
+              try {
+                await jobFn(...args);
+                success = true;
+                span.setStatus({
+                  code: 1, // OK
+                });
+              } catch (e) {
+                logError(e);
+                span.setStatus({
+                  code: 2, // ERROR
+                });
+              }
+
+              const duration = new Date().getTime() - start;
+
+              console.log(
+                `Job ${
+                  success ? "succeeded" : "failed"
+                } [${jobName} - ${jobId}] in ${(duration / 1000).toFixed(3)}s`,
+              );
+            },
+          );
         });
-        scope.setTransactionName(jobName);
-
-        return await Sentry.startSpan(
-          {
-            op: "process",
-            name: `faktory.${jobName.toLowerCase()}`,
-          },
-          async (span) => {
-            span.setAttribute("messaging.operation", "process");
-            span.setAttribute("messaging.system", "faktory");
-
-            console.log(`Running job [${jobName} - ${jobId}]`);
-            const start = new Date().getTime();
-            let success = false;
-            try {
-              await jobFn(...args);
-              success = true;
-              span.setStatus({
-                code: 1, // OK
-              });
-            } catch (e) {
-              logError(e);
-              span.setStatus({
-                code: 2, // ERROR
-              });
-            }
-
-            const duration = new Date().getTime() - start;
-
-            console.log(
-              `Job ${
-                success ? "succeeded" : "failed"
-              } [${jobName} - ${jobId}] in ${(duration / 1000).toFixed(3)}s`,
-            );
-          },
-        );
-      });
-    });
+      },
+    );
   };
 }
 
