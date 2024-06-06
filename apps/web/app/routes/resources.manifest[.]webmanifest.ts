8b2f3ab99d6aafc44e7c1624e08d73ebf193a7e6
David Cramer <dcramer@gmail.com>
2024-06-06 14:33:21 -0700

    Use uniform glyph

--- apps/web/app/routes/resources.manifest[.]webmanifest.ts
+++ apps/web/app/routes/resources.manifest[.]webmanifest.ts
@@ -1,6 +1,4 @@
 import glyphUrl from "@peated/web/assets/glyph.png";
-import icon192Url from "@peated/web/assets/icon192.png";
-import icon512Url from "@peated/web/assets/icon512.png";
 import config from "@peated/web/config";
 import { json } from "@remix-run/server-runtime";
 
@@ -20,7 +18,7 @@ export async function loader() {
           icons: [
             {
               src: glyphUrl,
-              sizes: "128x128",
+              sizes: "512x512 192x192 128x128 64x64 32x32 24x24 16x16",
               type: "image/png",
               purpose: "any monochrome",
             },
@@ -31,17 +29,6 @@ export async function loader() {
         {
           src: glyphUrl,
           sizes: "512x512 192x192 64x64 32x32 24x24 16x16",
-          type: "image/png",
-        },
-        {
-          src: icon192Url,
-          sizes: "192x192",
-          type: "image/png",
-          density: "4.0",
-        },
-        {
-          src: icon512Url,
-          sizes: "512x512",
           type: "image/png",
         },
       ],
