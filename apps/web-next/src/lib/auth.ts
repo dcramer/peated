411911431ff5d4989647c58b635cee884dc4b9aa
David Cramer <dcramer@gmail.com>
2024-06-20 16:24:42 +0200

    idk favorites page kind of works

--- apps/web-next/src/lib/auth.ts
+++ apps/web-next/src/lib/auth.ts
@@ -1,5 +1,6 @@
 import { type User } from "@peated/server/src/types";
 import { type SessionOptions } from "iron-session";
+import { redirect } from "next/navigation";
 
 export interface SessionData {
   user: User | null;
@@ -24,67 +25,30 @@ export const sessionOptions: SessionOptions = {
   },
 };
 
-export function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+export function getSafeRedirect(value: string | null) {
+  if (!value || value?.indexOf("/") !== 0 || value?.indexOf("//") === 0)
+    return "/";
+  return value;
 }
 
-// export async function getSession(request: Request | ExpressRequest) {
-//   const cookie =
-//     "get" in request
-//       ? request.get("Cookie")
-//       : (request as Request).headers.get("Cookie");
-//   return await sessionStorage.getSession(cookie);
-// }
+export function redirectToAuth({
+  pathname = "/",
+  search = "",
+}: {
+  pathname?: string;
+  search?: string;
+}) {
+  return redirect(getAuthRedirect({ pathname, search }));
+}
 
-// export async function getUser(
-//   session: Session<SessionPayload, SessionPayload>,
-// ) {
-//   return session.get("user");
-// }
+export function getAuthRedirect({
+  pathname = "/",
+  search = "",
+}: {
+  pathname?: string;
+  search?: string;
+}) {
+  const redirectTo = pathname + search;
 
-// export async function getAccessToken(
-//   session: Session<SessionPayload, SessionPayload>,
-// ) {
-//   return session.get("accessToken");
-// }
-
-// export async function createSession({
-//   request,
-//   session,
-//   remember = true,
-//   redirectTo = "/",
-// }: {
-//   request: Request;
-//   session: SessionPayload;
-//   remember?: boolean;
-//   redirectTo?: string | null;
-// }) {
-//   const s = await getSession(request);
-//   s.set("user", session.user);
-//   s.set("accessToken", session.accessToken);
-
-//   return redirectDocument(getSafeRedirect(redirectTo), {
-//     headers: {
-//       "Set-Cookie": await sessionStorage.commitSession(s, {
-//         maxAge: remember
-//           ? 60 * 60 * 24 * 7 // 7 days
-//           : undefined,
-//       }),
-//     },
-//   });
-// }
-
-// export async function logout(request: Request | ExpressRequest) {
-//   const s = await getSession(request);
-//   return redirectDocument("/", {
-//     headers: {
-//       "Set-Cookie": await sessionStorage.destroySession(s),
-//     },
-//   });
-// }
-
-// function getSafeRedirect(value: string | null) {
-//   if (!value || value?.indexOf("/") !== 0 || value?.indexOf("//") === 0)
-//     return "/";
-//   return value;
-// }
+  return `/login?redirectTo=${encodeURIComponent(redirectTo)}`;
+}
