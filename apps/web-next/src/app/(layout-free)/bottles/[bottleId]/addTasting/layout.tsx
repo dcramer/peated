c6b195a00b2271cea56bdb2542f27842be0a4178
David Cramer <dcramer@gmail.com>
2024-06-23 13:33:51 +0200

    Various cleanup and addTasting page

---
+++ apps/web-next/src/app/(layout-free)/bottles/[bottleId]/addTasting/layout.tsx
@@ -0,0 +1,10 @@
+import { type Metadata } from "next";
+import { type ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Record Tasting",
+};
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
--- apps/web-next/src/components/collectionAction.tsx
+++ apps/web-next/src/components/collectionAction.tsx
@@ -3,19 +3,46 @@
 import { StarIcon as StarIconFilled } from "@heroicons/react/20/solid";
 import { StarIcon } from "@heroicons/react/24/outline";
 import type { Bottle } from "@peated/server/types";
-import { trpc } from "@peated/web/lib/trpc";
+import { isTRPCClientError, trpc } from "@peated/web/lib/trpc";
+import useAuth from "../hooks/useAuth";
 import Button from "./button";
 
 export default function CollectionAction({ bottle }: { bottle: Bottle }) {
-  const [isCollected, { isLoading }] = trpc.collectionList.useSuspenseQuery(
-    {
-      bottle: bottle.id,
-      user: "me",
-    },
-    {
-      select: (data) => data.results.length > 0,
-    },
-  );
+  const { user } = useAuth();
+
+  if (!user) {
+    return (
+      <Button href="/login" color="primary">
+        <StarIcon className="h-4 w-4" aria-hidden="true" />
+      </Button>
+    );
+  }
+
+  let isCollected = false;
+  let isLoading = false;
+  try {
+    const [result, collectedQuery] = trpc.collectionList.useSuspenseQuery(
+      {
+        bottle: bottle.id,
+        user: "me",
+      },
+      {
+        select: (data) => data.results.length > 0,
+      },
+    );
+    isCollected = result;
+    isLoading = collectedQuery.isLoading;
+  } catch (err) {
+    if (isTRPCClientError(err) && err.data?.code === "UNAUTHORIZED") {
+      return (
+        <Button href="/login" color="primary">
+          <StarIcon className="h-4 w-4" aria-hidden="true" />
+        </Button>
+      );
+    }
+    throw err;
+  }
+
   const favoriteBottleMutation = trpc.collectionBottleCreate.useMutation();
   const unfavoriteBottleMutation = trpc.collectionBottleDelete.useMutation();
 
