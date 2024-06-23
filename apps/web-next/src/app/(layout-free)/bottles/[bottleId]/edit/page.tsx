2f304623b149f4d1d88e6312592f87f7c957bfea
David Cramer <dcramer@gmail.com>
2024-06-23 17:37:38 +0200

    convert bottle edit

---
+++ apps/web-next/src/app/(layout-free)/bottles/[bottleId]/edit/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import BottleForm from "@peated/web/components/bottleForm";
+import { useModRequired } from "@peated/web/hooks/useAuthRequired";
+import { trpc } from "@peated/web/lib/trpc";
+import { useRouter } from "next/navigation";
+
+export default function MergeBottle({
+  params: { bottleId },
+}: {
+  params: { bottleId: string };
+}) {
+  useModRequired();
+
+  const [bottle] = trpc.bottleById.useSuspenseQuery(Number(bottleId));
+  const trpcUtils = trpc.useUtils();
+
+  const router = useRouter();
+
+  const bottleUpdateMutation = trpc.bottleUpdate.useMutation({
+    onSuccess: (data) => {
+      if (!data) return;
+      const previous = trpcUtils.bottleById.getData(data.id);
+      if (previous) {
+        trpcUtils.bottleById.setData(data.id, {
+          ...previous,
+          ...data,
+        });
+      }
+    },
+  });
+
+  return (
+    <BottleForm
+      onSubmit={async (data) => {
+        await bottleUpdateMutation.mutateAsync(
+          {
+            bottle: bottle.id,
+            ...data,
+          },
+          {
+            onSuccess: () => router.push(`/bottles/${bottleId}`),
+          },
+        );
+      }}
+      initialData={bottle}
+      title="Edit Bottle"
+    />
+  );
+}
