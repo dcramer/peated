2637367af1ae2587597f1de567c17fc682c1fe61
David Cramer <dcramer@gmail.com>
2024-06-25 21:35:32 -0700

    Remove k8s provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,10 +14,6 @@ terraform {
       source  = "hashicorp/google-beta"
       version = "~> 5.33.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
-    }
   }
 }
 
@@ -37,12 +33,3 @@ provider "google-beta" {
   project = var.project_id
   region  = var.region
 }
-
-provider "kubernetes" {
-  host  = "https://${module.gke.endpoint}"
-  token = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(
-    module.gke.ca_certificate,
-  )
-}
-
