bba23546fec689b57d94cdd9e90bae8d576828ce
David Cramer <dcramer@gmail.com>
2024-07-07 17:37:56 -0700

    Fix validation

--- apps/server/src/lib/osm.ts
+++ apps/server/src/lib/osm.ts
@@ -4,9 +4,12 @@ export const OSMSchema = z.object({
   features: z.array(
     z.object({
       properties: z.object({
-        addresstype: z.enum(["country", "state", "county"]),
+        addresstype: z.union([
+          z.string(),
+          z.enum(["country", "state", "county"]),
+        ]),
         importance: z.number().gte(0).lte(1.0),
-        type: z.enum(["administrative"]),
+        type: z.union([z.string(), z.enum(["administrative"])]),
       }),
       geometry: z.object({
         type: z.enum(["Point"]),
