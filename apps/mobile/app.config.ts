f56f759ae2268fd7257dcbdd00a4fd49cbee0d79
David Cramer <dcramer@gmail.com>
2024-07-15 15:33:14 -0700

    Various restructure based on create-t3-turbo

---
+++ apps/mobile/app.config.ts
@@ -0,0 +1,48 @@
+import type { ConfigContext, ExpoConfig } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "peated",
+  slug: "peated",
+  scheme: "peated",
+  version: "0.1.0",
+  orientation: "portrait",
+  icon: "./src/assets/images/icon.png",
+  userInterfaceStyle: "automatic",
+  splash: {
+    image: "./src/assets/images/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  jsEngine: "hermes",
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./src/assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./src/assets/images/favicon.png",
+  },
+  experiments: {
+    tsconfigPaths: true,
+    typedRoutes: true,
+  },
+  plugins: [
+    "expo-router",
+    [
+      "@sentry/react-native/expo",
+      {
+        organization:
+          "sentry org slug, or use the `SENTRY_ORG` environment variable",
+        project:
+          "sentry project name, or use the `SENTRY_PROJECT` environment variable",
+      },
+    ],
+  ],
+});
