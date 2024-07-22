43ce28efc9f77e71e75f687c612e94f5505e96e9
David Cramer <dcramer@gmail.com>
2024-07-21 21:13:43 -0700

    Handle duplicate prices

--- apps/cli/src/commands/prices.ts
+++ apps/cli/src/commands/prices.ts
@@ -1,7 +1,7 @@
 import program from "@peated/cli/program";
 import { db } from "@peated/server/db";
-import { storePrices } from "@peated/server/db/schema";
-import { asc, eq } from "drizzle-orm";
+import { storePriceHistories, storePrices } from "@peated/server/db/schema";
+import { and, asc, eq, ne, sql } from "drizzle-orm";
 import { findBottleId } from "../../../server/src/lib/bottleFinder";
 import { normalizeBottle } from "../../../server/src/lib/normalize";
 
@@ -13,11 +13,7 @@ subcommand
   .action(async (options) => {
     const step = 1000;
     const baseQuery = db
-      .select({
-        id: storePrices.id,
-        bottleId: storePrices.bottleId,
-        name: storePrices.name,
-      })
+      .select()
       .from(storePrices)
       .orderBy(asc(storePrices.id));
 
@@ -26,7 +22,7 @@ subcommand
     while (hasResults) {
       hasResults = false;
       const query = await baseQuery.offset(offset).limit(step);
-      for (const { id, ...price } of query) {
+      for (const price of query) {
         const { name } = normalizeBottle({
           name: price.name,
           isFullName: true,
@@ -42,10 +38,49 @@ subcommand
 
           console.log(`M: ${price.name} -> ${JSON.stringify(values)}`);
           if (!options.dryRun) {
-            await db
-              .update(storePrices)
-              .set(values)
-              .where(eq(storePrices.id, id));
+            // TODO: move this code
+            try {
+              await db.transaction(async (tx) => {
+                return await tx
+                  .update(storePrices)
+                  .set(values)
+                  .where(eq(storePrices.id, price.id));
+              });
+            } catch (err: any) {
+              if (
+                err?.code === "23505" &&
+                err?.constraint === "store_price_unq_name"
+              ) {
+                await tx.transaction(async (tx) => {
+                  const [match] = await db
+                    .select({
+                      id: storePrices.id,
+                    })
+                    .from(storePrices)
+                    .where(
+                      and(
+                        eq(
+                          sql`LOWER(${storePrices.name})`,
+                          values.name.toLowerCase(),
+                        ),
+                        eq(storePrices.externalSiteId, price.externalSiteId),
+                        eq(storePrices.volume, price.volume),
+                        ne(storePrices.id, price.id),
+                      ),
+                    )
+                    .limit(1);
+                  await tx
+                    .update(storePriceHistories)
+                    .set({
+                      priceId: match.id,
+                    })
+                    .where(eq(storePriceHistories.priceId, price.id));
+                  await tx
+                    .delete(storePrices)
+                    .where(eq(storePrices.id, price.id));
+                });
+              }
+            }
           }
         }
         hasResults = true;
