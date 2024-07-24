b1204cd391c436b6b2c5d9a48c168c00c1e98c2f
David Cramer <dcramer@gmail.com>
2024-07-23 18:42:02 -0700

    Remove async top level

--- apps/server/src/worker/queues.ts
+++ apps/server/src/worker/queues.ts
@@ -3,13 +3,13 @@ import { Queue } from "bullmq";
 import IORedis from "ioredis";
 import config from "../config";
 
-export async function getConnection() {
+export function getConnection() {
   return new IORedis(config.REDIS_URL, {
     maxRetriesPerRequest: null,
   });
 }
 
-export const defaultConnection = await getConnection();
+export const defaultConnection = getConnection();
 
 export const defaultQueue = new Queue("default", {
   connection: defaultConnection,
