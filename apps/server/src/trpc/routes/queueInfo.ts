4c61ab0ad45ef0296ec6a6fa64d9e78d4d8dd34d
David Cramer <dcramer@gmail.com>
2024-07-23 19:22:25 -0700

    Ease readability

--- apps/server/src/trpc/routes/queueInfo.ts
+++ apps/server/src/trpc/routes/queueInfo.ts
@@ -2,12 +2,13 @@ import { getQueue } from "@peated/server/worker/client";
 import { adminProcedure } from "..";
 
 export default adminProcedure.query(async function () {
-  // const client = await getClient();
-  // const info = await client.info();
-  // return info;
-  const stats = await (
-    await getQueue("default")
-  ).getJobCounts("wait", "active", "completed", "failed");
+  const queue = await getQueue("default");
+  const stats = await queue.getJobCounts(
+    "wait",
+    "active",
+    "completed",
+    "failed",
+  );
 
   return { stats };
 });
