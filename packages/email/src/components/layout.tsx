a179f7260b7870b4eb3da6ab3a9acc252af9edaf
David Cramer <dcramer@gmail.com>
2024-07-26 09:19:42 -0700

    Switch out nested sections

--- packages/email/src/components/layout.tsx
+++ packages/email/src/components/layout.tsx
@@ -1,16 +1,6 @@
 import theme from "@peated/design";
 import tailwindConfig from "@peated/design/tailwind/base";
-import {
-  Body,
-  Column,
-  Container,
-  Head,
-  Html,
-  Img,
-  Link,
-  Section,
-  Tailwind,
-} from "jsx-email";
+import { Body, Container, Head, Html, Img, Section, Tailwind } from "jsx-email";
 import type { ReactNode } from "react";
 
 export default function Layout({
@@ -29,24 +19,15 @@ export default function Layout({
           style={{ backgroundColor: theme.colors.slate[900] }}
         >
           <Container className="max-w-[540px]">
-            <Container className="rounded border-solid border-slate-700 bg-slate-900 pb-[32px] lg:border">
-              <Section className="bg-highlight mb-[16px] rounded-t border border-solid border-slate-900 px-[16px] py-[16px]">
-                <Column className="text-center">
-                  <Link
-                    href={baseUrl}
-                    disableDefaultStyle
-                    className="no-underline"
-                  >
-                    <Img
-                      src={`${baseUrl}/assets/glyph-black.png`}
-                      width="96"
-                      height="96"
-                      alt="Peated"
-                      className="mx-auto"
-                      disableDefaultStyle
-                    />
-                  </Link>
-                </Column>
+            <Container className="rounded border-solid border-slate-700 bg-slate-900 lg:border">
+              <Section className="bg-highlight rounded-t border border-solid border-slate-900 px-[20px] py-[10px]">
+                <Img
+                  src={`${baseUrl}/assets/glyph-black.png`}
+                  width="96"
+                  height="96"
+                  alt="Peated"
+                  className="mx-auto my-0"
+                />
               </Section>
               {children}
             </Container>
