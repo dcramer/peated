39a5deed478850861313d888ddae334250242ecb
David Cramer <dcramer@gmail.com>
2024-07-31 10:50:17 -0700

    Move verify to client component so server action works

--- apps/web/src/app/(layout-free)/verify/page.tsx
+++ apps/web/src/app/(layout-free)/verify/page.tsx
@@ -1,50 +1,60 @@
+"use client";
+
 import Button from "@peated/web/components/button";
 import LayoutSplash from "@peated/web/components/layoutSplash";
+import Spinner from "@peated/web/components/spinner";
+import useAuth from "@peated/web/hooks/useAuth";
 import { updateSession } from "@peated/web/lib/auth.actions";
-import { getCurrentUser } from "@peated/web/lib/auth.server";
 import { logError } from "@peated/web/lib/log";
-import { isTRPCClientError } from "@peated/web/lib/trpc/client";
-import { getUnsafeTrpcClient } from "@peated/web/lib/trpc/client.server";
-import { redirect } from "next/navigation";
+import { isTRPCClientError, trpc } from "@peated/web/lib/trpc/client";
+import { redirect, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import ResendVerificationForm from "./resendForm";
 
-export default async function Verify({
-  searchParams,
-}: {
-  searchParams: Record<string, any>;
-}) {
-  let error = "";
-  let success = false;
+export default function Verify() {
+  const { user } = useAuth();
+  const searchParams = useSearchParams();
 
-  const user = await getCurrentUser();
-  if (user?.verified) {
-    return redirect("/");
-  }
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
 
-  const token = searchParams.token;
-  if (token) {
-    const trpcClient = await getUnsafeTrpcClient();
-    try {
-      await trpcClient.mutation("emailVerify", token);
+  const emailVerifyMutation = trpc.emailVerify.useMutation({
+    onSuccess: async () => {
+      setSuccess(true);
       await updateSession();
-      success = true;
-    } catch (err) {
-      if (isTRPCClientError(err)) {
-        error = err.message;
-      } else {
-        logError(err);
-        error = "An unknown internal error occurred.";
-      }
-    }
-  } else if (!user) {
-    return redirect("/login");
+    },
+  });
+
+  const token = searchParams.get("token") || "";
+  if (user?.verified || (!user && !token)) {
+    redirect("/");
   }
+
+  useEffect(() => {
+    emailVerifyMutation.mutate(token, {
+      onError: (err) => {
+        if (isTRPCClientError(err)) {
+          setError(err.message);
+        } else {
+          logError(err);
+          setError("An unknown internal error occurred.");
+        }
+        setLoading(false);
+      },
+      onSuccess: () => {
+        setLoading(false);
+      },
+    });
+  }, [token]);
 
   return (
     <LayoutSplash>
       <div className="mb-16 flex flex-col items-center">
         <h1 className="mb-4 text-2xl font-semibold">Account Verification</h1>
-        {error ? (
+        {loading ? (
+          <Spinner />
+        ) : error ? (
           <>
             <p className="mb-4 text-center">
               There was an error verifying your account.
