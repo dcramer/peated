087ddb508fa961488beac71560dfac31a266e363
David Cramer <dcramer@gmail.com>
2024-08-02 10:23:20 -0700

    Attempt to fix odd overflow

--- apps/web/src/app/(default)/users/[username]/userBadgeList.tsx
+++ apps/web/src/app/(default)/users/[username]/userBadgeList.tsx
@@ -13,28 +13,34 @@ export function UserBadgeList({ userId }: { userId: number }) {
   if (!awardList.results) return null;
 
   return (
-    <ul className="scrollbar-none flex justify-center gap-2 overflow-x-scroll lg:justify-start">
-      {awardList.results.map((award) => {
-        return (
-          <li
-            key={award.id}
-            title={award.badge.name}
-            className={classNames(
-              "group relative flex flex-shrink-0 flex-col items-center gap-y-1 rounded p-1 text-sm hover:bg-slate-800",
-              award.level === 0 ? "grayscale" : "",
-            )}
-          >
-            <Link
-              href={`/badges/${award.badge.id}`}
-              className="absolute inset-0"
-            />
-            <BadgeImage badge={award.badge} level={award.level} />
-            <div className="text-muted text-xs">
-              {award.level > 0 ? <>Level {award.level}</> : <em>Discovered</em>}
-            </div>
-          </li>
-        );
-      })}
-    </ul>
+    <div className="flex justify-center lg:justify-start">
+      <ul className="scrollbar-none flex gap-2 overflow-x-scroll">
+        {awardList.results.map((award) => {
+          return (
+            <li
+              key={award.id}
+              title={award.badge.name}
+              className={classNames(
+                "group relative flex flex-shrink-0 flex-col items-center gap-y-1 rounded p-1 text-sm hover:bg-slate-800",
+                award.level === 0 ? "grayscale" : "",
+              )}
+            >
+              <Link
+                href={`/badges/${award.badge.id}`}
+                className="absolute inset-0"
+              />
+              <BadgeImage badge={award.badge} level={award.level} />
+              <div className="text-muted text-xs">
+                {award.level > 0 ? (
+                  <>Level {award.level}</>
+                ) : (
+                  <em>Discovered</em>
+                )}
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
