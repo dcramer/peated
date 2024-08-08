0b840e7c75600b4ae19d304eb87386873210f576
David Cramer <dcramer@gmail.com>
2024-08-08 09:56:57 -0700

    Kill excess margins on fieldsets via spacing

--- apps/web/src/components/fieldset.tsx
+++ apps/web/src/components/fieldset.tsx
@@ -1,10 +1,5 @@
 import { type ComponentPropsWithoutRef } from "react";
 
 export default function Fieldset(props: ComponentPropsWithoutRef<"fieldset">) {
-  return (
-    <fieldset
-      className="relative space-y-4 border-y border-slate-800"
-      {...props}
-    />
-  );
+  return <fieldset className="relative border-y border-slate-800" {...props} />;
 }
