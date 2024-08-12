9603668fcf4dc6896de36b574322c33d8d46c555
David Cramer <dcramer@gmail.com>
2024-08-12 08:28:07 -0700

    MVP for search refactor

--- apps/web/src/app/(layout-free)/search/page.tsx
+++ apps/web/src/app/(layout-free)/search/page.tsx
@@ -1,16 +1,43 @@
-import { SearchPanel } from "@peated/web/components/search";
+import { getCurrentUser } from "@peated/web/lib/auth.server";
+import { getTrpcClient } from "@peated/web/lib/trpc/client.server";
 import { type Metadata } from "next";
+import SearchResults from "./results";
+
+const maxResults = 50;
 
 export const metadata: Metadata = {
   title: "Search",
 };
 
-export default function Page({
+export default async function Page({
   searchParams,
 }: {
   searchParams: Record<string, any>;
 }) {
-  const query = searchParams.q ?? "";
+  const user = await getCurrentUser();
 
-  return <SearchPanel initialValue={query} />;
+  const query = searchParams.q ?? "";
+  const directToTasting = searchParams.tasting !== undefined;
+
+  const isUserQuery = query.indexOf("@") !== -1 && user;
+
+  const include: ("bottles" | "entities" | "users")[] = [];
+  if (directToTasting || !isUserQuery) include.push("bottles");
+  if (!directToTasting && user && (isUserQuery || query)) include.push("users");
+  if (!directToTasting) include.push("entities");
+
+  const trpcClient = await getTrpcClient();
+  const { results } = await trpcClient.search.fetch({
+    query,
+    limit: maxResults,
+    include,
+  });
+
+  return (
+    <SearchResults
+      results={results}
+      query={query}
+      directToTasting={directToTasting}
+    />
+  );
 }
