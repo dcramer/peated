5cd1c9c3cb2dc40b890a5856b1ad67e2dace21ec
David Cramer <dcramer@gmail.com>
2024-08-23 12:59:23 -0700

    Improve test readability

--- apps/server/src/lib/bottleFinder.test.ts
+++ apps/server/src/lib/bottleFinder.test.ts
@@ -1,81 +1,85 @@
 import { findBottleId, findEntity } from "./bottleFinder";
 
-test("findBottle matches exact", async ({ fixtures }) => {
-  const bottle = await fixtures.Bottle({
-    name: "Test",
-    vintageYear: null,
-    releaseYear: null,
+describe("findBottleId", () => {
+  test("matches exact", async ({ fixtures }) => {
+    const bottle = await fixtures.Bottle({
+      name: "Test",
+      vintageYear: null,
+      releaseYear: null,
+    });
+    const result = await findBottleId(bottle.fullName);
+    expect(result).toMatchInlineSnapshot(`1`);
   });
-  const result = await findBottleId(bottle.fullName);
-  expect(result).toBe(bottle.id);
-});
 
-// test("findBottle matches fullName as prefix", async ({ fixtures }) => {
-//   const bottle = await fixtures.Bottle();
-//   const result = await findBottleId(bottle.fullName + " Single Grain");
-//   expect(result).toBe(bottle.id);
-// });
+  // test("matches fullName as prefix", async ({ fixtures }) => {
+  //   const bottle = await fixtures.Bottle();
+  //   const result = await findBottleId(bottle.fullName + " Single Grain");
+  //   expect(result).toBe(bottle.id);
+  // });
 
-test("findBottle will not wrongly match a suffix", async ({ fixtures }) => {
-  const brand = await fixtures.Entity({ name: "The Macallan" });
-  const bottle = await fixtures.Bottle({
-    brandId: brand.id,
-    name: "12-year-old Double Cask",
+  test("will not wrongly match a suffix", async ({ fixtures }) => {
+    const brand = await fixtures.Entity({ name: "The Macallan" });
+    const bottle = await fixtures.Bottle({
+      brandId: brand.id,
+      name: "12-year-old Double Cask",
+    });
+    const result = await findBottleId("The Macallan 12-year-old");
+    expect(result).toMatchInlineSnapshot(`null`);
   });
-  const result = await findBottleId("The Macallan 12-year-old");
-  expect(result).toEqual(null);
-});
 
-test("findBottle doesnt match random junk", async ({ fixtures }) => {
-  await fixtures.Bottle();
-  const result = await findBottleId("No Chance");
-  expect(result).toBe(null);
-});
-
-test("findBottle matches alias", async ({ fixtures }) => {
-  const bottle = await fixtures.Bottle();
-  await fixtures.BottleAlias({
-    bottleId: bottle.id,
-    name: "Something Silly",
+  test("doesnt match random junk", async ({ fixtures }) => {
+    await fixtures.Bottle();
+    const result = await findBottleId("No Chance");
+    expect(result).toMatchInlineSnapshot(`null`);
   });
-  const result = await findBottleId("Something Silly");
-  expect(result).toBe(bottle.id);
-});
 
-test("findBottle prioritizes correct prefix", async ({ fixtures }) => {
-  const entity = await fixtures.Entity({ name: "Aberfeldy" });
-  const bottle = await fixtures.Bottle({
-    brandId: entity.id,
-    name: "18-year-old",
+  test("matches alias", async ({ fixtures }) => {
+    const bottle = await fixtures.Bottle();
+    await fixtures.BottleAlias({
+      bottleId: bottle.id,
+      name: "Something Silly",
+    });
+    const result = await findBottleId("Something Silly");
+    expect(result).toMatchInlineSnapshot(`1`);
   });
-  const bottle2 = await fixtures.Bottle({
-    brandId: entity.id,
-    name: "18-year-old Port Cask",
+
+  test("prioritizes correct prefix", async ({ fixtures }) => {
+    const entity = await fixtures.Entity({ name: "Aberfeldy" });
+    const bottle = await fixtures.Bottle({
+      brandId: entity.id,
+      name: "18-year-old",
+    });
+    const bottle2 = await fixtures.Bottle({
+      brandId: entity.id,
+      name: "18-year-old Port Cask",
+    });
+    const result = await findBottleId("Aberfeldy 18-year-old Port Cask");
+    expect(result).toMatchInlineSnapshot(`2`);
+
+    const result2 = await findBottleId("Aberfeldy 18-year-old");
+    expect(result2).toMatchInlineSnapshot(`1`);
   });
-  const result = await findBottleId("Aberfeldy 18-year-old Port Cask");
-  expect(result).toBe(bottle2.id);
-
-  const result2 = await findBottleId("Aberfeldy 18-year-old");
-  expect(result2).toBe(bottle.id);
 });
 
-test("findEntity matches exact", async ({ fixtures }) => {
-  const entity = await fixtures.Entity({ name: "Hibiki" });
-  const result = await findEntity("Hibiki");
-  expect(result?.id).toEqual(entity.id);
-});
+describe("findEntity", () => {
+  test("matches exact", async ({ fixtures }) => {
+    const entity = await fixtures.Entity({ name: "Hibiki" });
+    const result = await findEntity("Hibiki");
+    expect(result?.id).toEqual(entity.id);
+  });
 
-test("findEntity matches bottle name prefix", async ({ fixtures }) => {
-  const entity = await fixtures.Entity({ name: "Hibiki" });
-  const result = await findEntity("Hibiki 12-year-old");
-  expect(result?.id).toEqual(entity.id);
-});
+  test("matches bottle name prefix", async ({ fixtures }) => {
+    const entity = await fixtures.Entity({ name: "Hibiki" });
+    const result = await findEntity("Hibiki 12-year-old");
+    expect(result?.id).toEqual(entity.id);
+  });
 
-test("findEntity does not match entity name prefix", async ({ fixtures }) => {
-  await fixtures.Entity({ name: "Hibiki Real" });
-  const result = await findEntity("Hibiki 12-year-old");
-  expect(result).toBeNull();
+  test("does not match entity name prefix", async ({ fixtures }) => {
+    await fixtures.Entity({ name: "Hibiki Real" });
+    const result = await findEntity("Hibiki 12-year-old");
+    expect(result).toBeNull();
 
-  await findEntity("The Hibiki Real 12-year-old");
-  expect(result).toBeNull();
+    await findEntity("The Hibiki Real 12-year-old");
+    expect(result).toBeNull();
+  });
 });
