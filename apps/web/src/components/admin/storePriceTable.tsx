43010f3b45271d67efaa1d3bfd6e9fd2e81647a1
David Cramer <dcramer@gmail.com>
2024-08-23 14:01:52 -0700

    Add updated to table

--- apps/web/src/components/admin/storePriceTable.tsx
+++ apps/web/src/components/admin/storePriceTable.tsx
@@ -2,6 +2,7 @@ import type { Bottle, PagingRel, StorePrice } from "@peated/server/types";
 import Link from "@peated/web/components/link";
 import Price from "@peated/web/components/price";
 import PaginationButtons from "../paginationButtons";
+import TimeSince from "../timeSince";
 
 export default function StorePriceTable({
   priceList,
@@ -31,6 +32,12 @@ export default function StorePriceTable({
               className="hidden px-3 py-2.5 text-right sm:table-cell"
             >
               Price
+            </th>
+            <th
+              scope="col"
+              className="hidden px-3 py-2.5 text-center sm:table-cell"
+            >
+              Updated
             </th>
           </tr>
         </thead>
@@ -65,6 +72,9 @@ export default function StorePriceTable({
                 </td>
                 <td className="hidden px-3 py-3 text-right sm:table-cell">
                   <Price value={price.price} currency={price.currency} />
+                </td>
+                <td className="hidden px-3 py-3 text-center sm:table-cell">
+                  <TimeSince date={price.updatedAt} />
                 </td>
               </tr>
             );
