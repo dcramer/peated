ce1d7688f5312018ba80fd004c288d4daf3a48c4
David Cramer <dcramer@gmail.com>
2024-08-23 20:49:07 -0700

    Handle numeric query params

--- apps/web/src/app/(admin)/admin/(default)/queue/page.tsx
+++ apps/web/src/app/(admin)/admin/(default)/queue/page.tsx
@@ -6,6 +6,7 @@ import { useFlashMessages } from "@peated/web/components/flash";
 import Link from "@peated/web/components/link";
 import SimpleHeader from "@peated/web/components/simpleHeader";
 import Table from "@peated/web/components/table";
+import useApiQueryParams from "@peated/web/hooks/useApiQueryParams";
 import { trpc, type RouterOutputs } from "@peated/web/lib/trpc/client";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useState } from "react";
@@ -14,10 +15,15 @@ import BottleSelector from "./bottleSelector";
 export default function Page() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const [aliasList] = trpc.bottleAliasList.useSuspenseQuery({
-    ...Object.fromEntries(searchParams.entries()),
-    onlyUnknown: true,
+
+  const queryParams = useApiQueryParams({
+    numericFields: ["cursor", "limit"],
+    overrides: {
+      onlyUnknown: true,
+    },
   });
+
+  const [aliasList] = trpc.bottleAliasList.useSuspenseQuery(queryParams);
   const [unmatchedBottle, setUnmatchedBottle] = useState<
     null | RouterOutputs["bottleAliasList"]["results"][number]
   >(null);
