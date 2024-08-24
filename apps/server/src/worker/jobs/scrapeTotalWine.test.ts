6a5eb1d65ede268b53aea4f13638e5c8fe0d086c
David Cramer <dcramer@gmail.com>
2024-08-24 12:27:06 -0700

    Adj snapshot

--- apps/server/src/worker/jobs/scrapeTotalWine.test.ts
+++ apps/server/src/worker/jobs/scrapeTotalWine.test.ts
@@ -16,14 +16,14 @@ test("simple", async ({ axiosMock }) => {
 
   await fn;
 
-  expect(items.length).toMatchInlineSnapshot(`104`);
+  expect(items.length).toMatchInlineSnapshot(`109`);
   expect(items[0]).toMatchInlineSnapshot(`
     {
       "currency": "usd",
       "name": "Grangestone Bourbon Cask Finish Single Malt Scotch Whisky",
-      "price": 3299,
-      "url": "https://www.totalwine.com/spirits/deals/scotch/single-malt/grangestone-bourbon-cask-finish-single-malt-scotch-whisky/p/135113750?s=1203&igrules=true",
-      "volume": 750,
+      "price": 6499,
+      "url": "https://www.totalwine.com/spirits/scotch/single-malt/grangestone-bourbon-cask-finish-single-malt-scotch-whisky/p/135113175?s=1203&igrules=true",
+      "volume": 1750,
     }
   `);
 });
