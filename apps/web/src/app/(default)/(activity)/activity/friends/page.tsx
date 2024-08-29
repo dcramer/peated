870b9b51425a77cc2dbe1f800887c79fbea325f3
David Cramer <dcramer@gmail.com>
2024-08-29 09:11:05 -0700

    Auth check on friends activity

--- apps/web/src/app/(default)/(activity)/activity/friends/page.tsx
+++ apps/web/src/app/(default)/(activity)/activity/friends/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import ActivityFeed from "@peated/web/components/activityFeed";
+import useAuthRequired from "@peated/web/hooks/useAuthRequired";
 import { trpc } from "@peated/web/lib/trpc/client";
 
 export const fetchCache = "default-no-store";
 
 export default function Page() {
+  useAuthRequired();
+
   const filter = "friends";
   const [tastingList] = trpc.tastingList.useSuspenseQuery({
     filter,
