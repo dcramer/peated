6e41778fc7f673eb1e457750eaa097e14b0142b0
David Cramer <dcramer@gmail.com>
2024-10-08 09:49:17 -0700

    Fix worker behavior for ESM

--- apps/server/src/run-worker.ts
+++ apps/server/src/run-worker.ts
@@ -4,9 +4,7 @@ import "./sentry";
 import * as Sentry from "@sentry/node";
 import { runWorker } from "./worker/client";
 
-if (typeof require !== "undefined" && require.main === module) {
-  runWorker().catch((e) => {
-    Sentry.captureException(e);
-    console.error("Worker crashed", e);
-  });
-}
+runWorker().catch((e) => {
+  Sentry.captureException(e);
+  console.error("Worker crashed", e);
+});
