92e5209be01d4239c4cf1823f93c0e11f347f5f9
David Cramer <dcramer@gmail.com>
2024-11-26 11:22:47 -0800

    Some initial draft code for tanstack start

---
+++ apps/web-tan/tailwind.config.cjs
@@ -0,0 +1,7 @@
+import preset from "@peated/design/tailwind/web";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  presets: [preset],
+};
