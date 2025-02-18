bcfeeebe6e3e2755a4abd0defcb73dd72989206a
David Cramer <dcramer@gmail.com>
2025-02-18 15:54:00 -0800

    oops

--- apps/web/src/app/(admin)/admin/(default)/layout.tsx
+++ apps/web/src/app/(admin)/admin/(default)/layout.tsx
@@ -2,7 +2,7 @@ import AdminSidebar from "@peated/web/components/admin/sidebar";
 import Layout from "@peated/web/components/layout";
 import React from "react";
 
-export default async function Layout({
+export default async function AdminLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
