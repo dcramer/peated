13b2d2b4a09356ab58edbe25ebb1a1f49b6cac9c
David Cramer <dcramer@gmail.com>
2025-03-04 17:00:15 -0800

    Seems this doesnt exist anymore

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -38,9 +38,6 @@ export default withSentryConfig(nextConfig, {
     },
   },
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
