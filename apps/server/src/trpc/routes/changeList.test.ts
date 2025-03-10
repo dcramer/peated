13e118cbdf9e4480776c3331cfcead589bb8bf3b
David Cramer <dcramer@gmail.com>
2025-03-09 22:08:16 -0700

    Prevent conflicts

--- apps/server/src/trpc/routes/changeList.test.ts
+++ apps/server/src/trpc/routes/changeList.test.ts
@@ -1,8 +1,8 @@
 import { createCaller } from "../router";
 
 test("lists changes", async ({ defaults, fixtures }) => {
-  await fixtures.Entity();
-  await fixtures.Entity();
+  await fixtures.Entity({ name: "Entity 1" });
+  await fixtures.Entity({ name: "Entity 2" });
 
   const caller = createCaller({ user: defaults.user });
   const { results } = await caller.changeList();
