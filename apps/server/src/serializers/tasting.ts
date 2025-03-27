eb537c7401f13bcc9fd4eb4b325617ecb0b20061
David Cramer <dcramer@gmail.com>
2025-03-26 19:19:25 -0700

    oops

--- apps/server/src/serializers/tasting.ts
+++ apps/server/src/serializers/tasting.ts
@@ -79,7 +79,9 @@ export const TastingSerializer = serializer({
           results.map((r) => r.release).filter(notEmpty),
           currentUser,
         )
-      ).map((data, index) => [results[index].id, data]),
+      )
+        .filter((data, index) => results[index].release)
+        .map((data, index) => [results[index].id, data]),
     );
 
     // TODO: combine friends + createdBy
