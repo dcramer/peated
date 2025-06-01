0bad1540db0c2c3d79de1eefbab805e7e5802fcc
David Cramer <dcramer@gmail.com>
2025-06-01 15:18:25 -0700

    It doesnt like my health server route

--- apps/web/src/routes/_health.tsx
+++
@@ -1,7 +0,0 @@
-import { createServerFn } from "@tanstack/react-start";
-
-export const ServerRoute = createServerFileRoute().methods({
-  GET: async () => {
-    return Response.json({ ok: true });
-  },
-});
