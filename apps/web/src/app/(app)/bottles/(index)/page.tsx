8b15c11bf28c7dc10e7c6abba67038b209984497
David Cramer <dcramer@gmail.com>
2026-08-31 10:13:25 -0700

    fix(bottles): default catalog to release sorting

--- apps/web/src/app/(app)/bottles/(index)/page.tsx
+++ apps/web/src/app/(app)/bottles/(index)/page.tsx
@@ -19,6 +19,7 @@ export default async function BottleListPage(props: {
 }) {
   const queryParams = normalizeBottleCatalogQueryParams(
     getApiQueryParams(await props.searchParams, {
+      defaults: { sort: "-release" },
       allowedValues: BOTTLE_CATALOG_ALLOWED_VALUES,
       fields: BOTTLE_CATALOG_QUERY_FIELDS,
       numericFields: [
